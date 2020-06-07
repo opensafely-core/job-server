a16c440ffb28dffb4c5e64e075ebf3d510272526
Seb Bacon <seb.bacon@gmail.com>
2020-06-07 21:52:43 +0100

    Add a hard-coded Slack notification

--- jobserver/api/models.py
+++ jobserver/api/models.py
@@ -1,6 +1,11 @@
 import datetime
+import os
+
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.db.models.signals import post_save
+
+from slack import WebClient
 
 
 class Job(models.Model):
@@ -13,6 +18,7 @@ class Job(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     started_at = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
+    request_ref = models.CharField(max_length=20, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if self.completed_at:
@@ -24,3 +30,19 @@ class Job(models.Model):
                 raise ValidationError("Cannot complete a job that has not started ")
             self.completed_at = datetime.datetime.now()
         super().save(*args, **kwargs)
+
+
+def notify_slack(sender, instance, created, raw, using, update_fields, **kwargs):
+    """Send a message to slack about the job
+    """
+    if sender == Job and instance.completed_at:
+        client = WebClient(token=os.environ["SLACK_API_TOKEN"])
+        msg = f"Operation {instance.operation} completed with status {instance.status_code}."
+        if instance.output_url:
+            msg += f" Output can be found at {instance.output_url}"
+        client.chat_postMessage(
+            channel="#bottest", text=msg, thread_ts=instance.request_ref
+        )
+
+
+post_save.connect(notify_slack)
--- jobserver/settings.py
+++ jobserver/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "jobserver.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default="sqlite://db.sqlite3", conn_max_age=600)
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3", conn_max_age=600)
 }
 
 
