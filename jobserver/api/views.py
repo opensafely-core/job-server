50d0f3dae284f63bc10b11bab709c3f3f6269bac
Seb Bacon <seb.bacon@gmail.com>
2020-07-30 10:20:49 +0100

    feat: use offset/limit pagination so we can pop single jobs

--- jobserver/api/views.py
+++ jobserver/api/views.py
@@ -9,5 +9,6 @@ class JobViewSet(viewsets.ModelViewSet):
     API endpoint that allows jobs to be viewed or edited.
     """
 
+    queryset = Job.objects.all().order_by("-created_at")
     serializer_class = JobSerializer
     filterset_fields = ("started", "repo", "backend", "db", "tag", "operation")
--- jobserver/settings.py
+++ jobserver/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "PAGE_SIZE": 100,
 }
