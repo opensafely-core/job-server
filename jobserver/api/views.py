0624220ce21f72a2fea740d1346fcb98719a0483
Seb Bacon <seb.bacon@gmail.com>
2020-07-30 10:20:22 +0100

    feat: use django-filter for more flexible querying

--- jobserver/api/views.py
+++ jobserver/api/views.py
@@ -10,24 +10,4 @@ class JobViewSet(viewsets.ModelViewSet):
     """
 
     serializer_class = JobSerializer
-
-    def get_queryset(self):
-        """
-        Optionally restricts the returned jobs based on any `started` param
-        """
-        queryset = Job.objects.all().order_by("-created_at")
-        qs_filter = {}
-        if "started" in self.request.query_params:
-            started_param = self.request.query_params.get("started", "False").lower()
-            if started_param == "true":
-                qs_filter["started"] = True
-            elif started_param == "false":
-                qs_filter["started"] = False
-        if "repo" in self.request.query_params:
-            qs_filter["repo"] = self.request.query_params["repo"]
-        if "backend" in self.request.query_params:
-            qs_filter["backend"] = self.request.query_params["backend"]
-        if qs_filter:
-            return queryset.filter(**qs_filter)
-        else:
-            return queryset.all()
+    filterset_fields = ("started", "repo", "backend", "db", "tag", "operation")
--- jobserver/settings.py
+++ jobserver/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "jobserver.api",
 ]
 
@@ -129,6 +130,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "PAGE_SIZE": 100,
 }
 
