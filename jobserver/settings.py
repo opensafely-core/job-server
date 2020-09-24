ac3609bfd0e123cbefe35bed7f539510f1726504
George Hickman <george@ghickman.co.uk>
2020-09-24 09:22:51 +0100

    configure templates directory for apps

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "jobserver.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
