06e870c9750311f6cfd13553ee0af658dd951ad0
George Hickman <george@ghickman.co.uk>
2020-09-24 16:41:02 +0100

    Turn debug off by default

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["OPENSAFELY_JOB_SERVER_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", default=False))
 
 ALLOWED_HOSTS = ["*"]
 
