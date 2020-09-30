54a15b1801d8ffcdda0f84215f23140e30f9fbca
George Hickman <george@ghickman.co.uk>
2020-09-30 13:20:32 +0100

    Add tests for Job model

--- tests/jobserver/test_models.py
+++ tests/jobserver/test_models.py
@@ -1,9 +1,148 @@
 from datetime import timedelta
 
 import pytest
+import responses
+from django.urls import reverse
 from django.utils import timezone
 
 from ..factories import JobFactory
+
+
+@pytest.mark.django_db
+def test_job_get_absolute_url():
+    job = JobFactory()
+
+    url = job.get_absolute_url()
+
+    assert url == reverse("job-detail", kwargs={"pk": job.pk})
+
+
+@pytest.mark.django_db
+@responses.activate
+def test_job_notify_callback_url_action_failed():
+    responses.add(responses.POST, "http://example.com", status=201)
+
+    JobFactory(
+        callback_url="http://example.com",
+        action_id="Research",
+        completed_at=timezone.now(),
+        started=True,
+        status_code=1,
+        status_message="test",
+    )
+
+    assert len(responses.calls) == 1
+    assert (
+        responses.calls[0].request.body
+        == b'{"message": "Error in Research (status test)"}'
+    )
+
+
+@pytest.mark.django_db
+@responses.activate
+def test_job_notify_callback_url_action_finished():
+    responses.add(responses.POST, "http://example.com", status=201)
+
+    JobFactory(
+        callback_url="http://example.com",
+        action_id="Research",
+        completed_at=timezone.now(),
+        started=True,
+        status_code=0,
+        status_message="test",
+    )
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.body == b'{"message": "Research finished: test"}'
+
+
+@pytest.mark.django_db
+@responses.activate
+def test_job_notify_callback_url_starting_dependency():
+    responses.add(responses.POST, "http://example.com", status=201)
+
+    parent = JobFactory()
+
+    job = JobFactory(
+        callback_url="http://example.com",
+        action_id="Research",
+        needed_by=parent,
+        started=False,
+    )
+
+    assert len(responses.calls) == 1
+
+    expected = f'{{"message": "Starting dependency Research, job#{job.pk}"}}'.encode()
+    assert responses.calls[0].request.body == expected
+
+
+@pytest.mark.django_db
+@responses.activate
+def test_job_notify_callback_url_started_with_no_parent():
+    responses.add(responses.POST, "http://example.com", status=201)
+
+    JobFactory(callback_url="http://example.com", started=False)
+
+    assert len(responses.calls) == 0
+
+
+@pytest.mark.django_db
+@responses.activate
+def test_job_notify_callback_url_with_no_callback_url():
+    responses.add(responses.POST, "http://example.com", status=201)
+
+    JobFactory()
+
+    assert len(responses.calls) == 0
+
+
+@pytest.mark.django_db
+def test_job_runtime():
+    duration = timedelta(hours=1, minutes=2, seconds=3)
+    started_at = timezone.now() - duration
+    job = JobFactory(started_at=started_at, completed_at=timezone.now())
+
+    assert job.runtime.hours == 1
+    assert job.runtime.minutes == 2
+    assert job.runtime.seconds == 3
+
+
+@pytest.mark.django_db
+def test_job_runtime_not_completed():
+    job = JobFactory(started_at=timezone.now())
+
+    # an unfinished job has no runtime
+    assert job.runtime is None
+
+
+@pytest.mark.django_db
+def test_job_runtime_not_started():
+    job = JobFactory()
+
+    # an unstarted job has no runtime
+    assert job.runtime is None
+
+
+@pytest.mark.django_db
+def test_job_save(freezer):
+    job = JobFactory()
+
+    assert job.started_at is None
+    assert job.completed_at is None
+
+    job.started = True
+    job.status_code = 1
+    job.save()
+
+    assert job.started_at == timezone.now()
+    assert job.completed_at == timezone.now()
+
+
+@pytest.mark.django_db
+def test_job_str():
+    job = JobFactory(action_id="Run")
+
+    assert str(job) == f"Run ({job.pk})"
 
 
 @pytest.mark.django_db
@@ -47,5 +186,4 @@ def test_job_status_status_6_is_pending():
 @pytest.mark.django_db
 def test_job_status_unstarted_is_pending():
     job = JobFactory()
-
     assert job.status == "Pending"
