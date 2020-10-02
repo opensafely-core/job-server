eeedad48940231f727398e7b6d6bde33cd9376ec
George Hickman <george@ghickman.co.uk>
2020-10-02 09:45:51 +0100

    Add status name as a title to status icons

--- jobserver/templatetags/status_tools.py
+++ jobserver/templatetags/status_tools.py
@@ -44,7 +44,7 @@ def status_icon(status_name):
 
     # Wrap to the SVG so we can style it neatly
     icon = f"""
-    <div style="color:{status['color']};width:1rem;">
+    <div style="color:{status['color']};width:1rem;" title="{status_name}">
         {svg}
     </div>
     """
