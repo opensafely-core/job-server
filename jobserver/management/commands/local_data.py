011443c4ce6f6d6e2c6dc00c4ed2e11da0ed4a20
George Hickman <george@ghickman.co.uk>
2020-10-05 09:29:57 +0100

    Create JobRequests in local dev

--- jobserver/management/commands/local_data.py
+++ jobserver/management/commands/local_data.py
@@ -4,54 +4,184 @@ from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from jobserver.models import Job, Workspace
+from jobserver.models import Job, JobRequest, User, Workspace
+
+
+actions = [
+    "run",
+    "process",
+    "hide",
+    "leap",
+    "science",
+]
+
+
+def request_1(workspace, user, now):
+    """
+    Completed JobRequest
+
+    Each related Job completed without error.
+    """
+    request1 = JobRequest.objects.create(created_by=user, workspace=workspace)
+
+    job1 = Job.objects.create(
+        request=request1,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=9),
+        completed_at=now + timedelta(minutes=10),
+    )
+    job2 = Job.objects.create(
+        request=request1,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=7),
+        completed_at=now + timedelta(minutes=8),
+    )
+    job3 = Job.objects.create(
+        request=request1,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=5),
+        completed_at=now + timedelta(minutes=6),
+    )
+    job4 = Job.objects.create(
+        request=request1,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=3),
+        completed_at=now + timedelta(minutes=4),
+    )
+    job5 = Job.objects.create(
+        request=request1,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=1),
+        completed_at=now + timedelta(minutes=2),
+    )
+
+    # Set up dependency links
+    job1.needed_by = job2
+    job1.save()
+
+    job2.needed_by = job3
+    job2.save()
+
+    job3.needed_by = job4
+    job3.save
+
+    job4.needed_by = job5
+    job4.save()
+
+
+def request_2(workspace, user, now):
+    """
+    Running JobRequest
+
+    One Job has completed, one is currently running, and one is pending.
+    """
+    request2 = JobRequest.objects.create(created_by=user, workspace=workspace)
+
+    job1 = Job.objects.create(
+        request=request2,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now,
+        completed_at=now + timedelta(minutes=1),
+    )
+    job2 = Job.objects.create(
+        request=request2,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=2),
+    )
+    job3 = Job.objects.create(
+        request=request2, workspace=workspace, action_id=random.choice(actions)
+    )
+
+    # Set up dependency links
+    job1.needed_by = job2
+    job1.save()
+
+    job2.needed_by = job3
+    job2.save()
+
+
+def request_3(workspace, user, now):
+    """
+    Failed JobRequest
+
+    One Job completed, one failed, and the final one failed because the second
+    one failed.
+    """
+    # JobRequest 3 - failed
+    request3 = JobRequest.objects.create(created_by=user, workspace=workspace)
+
+    job1 = Job.objects.create(
+        request=request3,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        started_at=now + timedelta(minutes=3),
+        completed_at=now + timedelta(minutes=19),
+    )
+    job2 = Job.objects.create(
+        request=request3,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        status_code=1,  # this job failed
+    )
+    job3 = Job.objects.create(
+        request=request3,
+        workspace=workspace,
+        action_id=random.choice(actions),
+        status_code=7,  # this job didn't run because the previous one failed
+    )
+
+    # Set up dependency links
+    job1.needed_by = job2
+    job1.save()
+
+    job2.needed_by = job3
+    job2.save()
+
+
+def request_4(workspace, user, now):
+    """
+    Pending JobRequest
+
+    All Jobs are Pending.
+    """
+    request4 = JobRequest.objects.create(created_by=user, workspace=workspace)
+
+    job1 = Job.objects.create(
+        request=request4,
+        workspace=workspace,
+        action_id=random.choice(actions),
+    )
+    job2 = Job.objects.create(
+        request=request4,
+        workspace=workspace,
+        action_id=random.choice(actions),
+    )
+
+    # Set up dependency links
+    job1.needed_by = job2
+    job1.save()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        """
-        Create 20 Jobs for each Status
-        """
-
-        actions = [
-            "run",
-            "process",
-            "hide",
-            "leap",
-            "science",
-        ]
+        Job.objects.all().delete()
+        JobRequest.objects.all().delete()
 
         workspace, _ = Workspace.objects.get_or_create(
             name="Atlantis", repo="atlantis", branch="tauri"
         )
 
+        george = User.objects.first()
         now = timezone.now() - timedelta(hours=1)
 
-        for _ in range(20):
-            now = now + timedelta(minutes=1)
-
-            # Completed
-            Job.objects.create(
-                workspace=workspace,
-                action_id=random.choice(actions),
-                started_at=now,
-                completed_at=timezone.now(),
-            )
-
-            # Dependency Failed
-            Job.objects.create(
-                workspace=workspace, action_id=random.choice(actions), status_code=7
-            )
-
-            # Failed
-            Job.objects.create(
-                workspace=workspace, action_id=random.choice(actions), status_code=1
-            )
-
-            # In Progress
-            Job.objects.create(
-                workspace=workspace, action_id=random.choice(actions), started_at=now
-            )
-
-            # Pending
-            Job.objects.create(workspace=workspace, action_id=random.choice(actions))
+        request_1(workspace, george, now)
+        request_2(workspace, george, now)
+        request_3(workspace, george, now)
+        request_4(workspace, george, now)
