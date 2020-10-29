d63298cce207c288831230150669dc465b227f02
George Hickman <george@ghickman.co.uk>
2020-10-29 11:44:30 +0000

    Display stats in a small banner on the site

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "jobserver.context_processors.nav",
+                "jobserver.context_processors.site_stats",
             ],
         },
     },
