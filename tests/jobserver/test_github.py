4355dacb7bba544e792f8fb31554469135d4182d
George Hickman <george@ghickman.co.uk>
2020-11-06 10:39:11 +0000

    test github.get_branch_sha

--- tests/jobserver/test_github.py
+++ tests/jobserver/test_github.py
@@ -1,6 +1,31 @@
 import responses
 
-from jobserver.github import get_file, get_repos_with_branches
+from jobserver.github import get_branch_sha, get_file, get_repos_with_branches
+
+
+@responses.activate
+def test_get_branch_sha():
+    data = {
+        "commit": {
+            "sha": "abc123",
+        }
+    }
+
+    expected_url = "https://api.github.com/repos/opensafely/some_repo/branches/master"
+    responses.add(responses.GET, expected_url, json=data, status=200)
+
+    output = get_branch_sha("some_repo", "master")
+
+    assert len(responses.calls) == 1
+
+    call = responses.calls[0]
+
+    # check the headers are correct
+    assert "token" in call.request.headers["Authorization"]
+    assert call.request.headers["Accept"] == "application/vnd.github.v3+json"
+    assert call.response.text == '{"commit": {"sha": "abc123"}}'
+
+    assert output == "abc123"
 
 
 @responses.activate
