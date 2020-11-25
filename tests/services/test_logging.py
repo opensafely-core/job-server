2bf82256033dca3af390e78cb9afe504319dc601
George Hickman <george@ghickman.co.uk>
2020-11-25 13:04:07 +0000

    Test services module

---
+++ tests/services/test_logging.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from services.logging import timestamper
+
+
+def test_timestamper_with_debug(monkeypatch, freezer):
+    monkeypatch.setattr("services.logging.DEBUG", True)
+
+    log = timestamper(None, None, {"event": "derp"})
+    assert log == {"event": "derp", "timestamp": datetime.now().isoformat() + "Z"}
+
+
+def test_timestamper_without_debug(monkeypatch):
+    monkeypatch.setattr("services.logging.DEBUG", False)
+
+    assert timestamper(None, None, {"event": "derp"}) == {"event": "derp"}
