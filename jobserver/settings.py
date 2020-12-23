2489a21d85fd6f9256bac477b6623bca648d31e6
George Hickman <george@ghickman.co.uk>
2020-12-23 14:42:49 +0000

    Shorten secret key config name
    This is only set in environments scoped to job-server.

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("OPENSAFELY_JOB_SERVER_SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
