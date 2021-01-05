4851608ba4030b7be9794535ed67fa51859dddf1
George Hickman <george@ghickman.co.uk>
2021-01-05 11:01:29 +0000

    Fix duplicate migration naming

--- jobserver/migrations/0055_add_workspace_is_archived.py
+++ jobserver/migrations/0055_add_workspace_is_archived.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("jobserver", "0053_add_backend_auth_token"),
+        ("jobserver", "0054_remove_blankability_from_job_timestamp_fields"),
     ]
 
     operations = [
