034d9fa391687b5da181ba072aae190ccb608f6c
George Hickman <george@ghickman.co.uk>
2021-02-10 11:08:16 +0000

    Ensure User.notifications_email is set on initial signup

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -203,5 +203,18 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+# Python/Django Social Auth
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+    "jobserver.pipeline.set_notifications_email",
+]
+
 # Sentry
 initialise_sentry()
