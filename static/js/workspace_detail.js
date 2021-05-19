2289ec2d0865518ce1a66524cb4749914638730d
George Hickman <george@ghickman.co.uk>
2021-05-19 15:16:41 +0100

    Use selected backends to filter the status icon update script

--- static/js/workspace_detail.js
+++ static/js/workspace_detail.js
@@ -1,27 +1,41 @@
-const updateStatuses = (url) => {
+const updateStatuses = (urlBase) => {
   /**
    * Update statuses for the current Workspace
    */
+
+  // if a backend radio button has been checked use it filter the API query
+  const checkedBackends = Array.from(document.getElementsByName('backend')).filter(e => e.checked);
+  let backendQuery = "";
+  if (checkedBackends.length > 0) {
+    backendQuery = `?backend=${checkedBackends[0].value}`;
+  }
+
+  const url = `${urlBase}${backendQuery}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      for (const [action, status] of Object.entries(data)) {
-        const icon = document.querySelector(`#heading-${action} .status-icon`);
+      const actions = document.querySelectorAll("#actions .card>.card-header");
 
-        if (icon === null) {
-          // icon can be null when we have jobs for an action which was
-          // previously in the project.yaml but has since been removed.
-          continue;
+      // loop the actions we've loaded from GitHub clearing their icon then
+      // setting an icon if we get a status for them
+      for (const action of actions) {
+        // reset status icon
+        const icon = action.querySelector(".status-icon");
+        icon.className = "status-icon";
+
+        const actionName = action.querySelector("code").title;
+        const status = data[actionName];
+        if (status) {
+          // the API has a status for this action, update the icon
+          icon.setAttribute("title", status);
+          icon.className = `status-icon ${status}`;
         }
-
-        icon.setAttribute("title", status);
-        icon.className = `status-icon ${status}`;
       }
     });
 }
 window.addEventListener("DOMContentLoaded", () => {
-  const url = document.getElementById("apiUrl").textContent;
+  const urlBase = document.getElementById("apiUrl").textContent;
 
   // poll the backend every 10s
-  window.setInterval(updateStatuses, 10000, url);
+  window.setInterval(updateStatuses, 10000, urlBase);
 });
