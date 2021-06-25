e81aef9ad46b72c43c41ffba7ae2a5a3b6f1fa83
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-06-25 13:36:48 +0100

    Copy vendor assets to dist/ folder

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import copy from "rollup-plugin-copy";
+
 /**
  * @type {import('vite').UserConfig}
  */
@@ -13,7 +15,37 @@ const config = {
     outDir: "assets/dist",
     emptyOutDir: true,
   },
-  plugins: [],
+  plugins: [
+    copy({
+      targets: [
+        {
+          src: "./node_modules/bootstrap/dist/css/bootstrap.min.*",
+          dest: "./assets/dist/vendor",
+        },
+        {
+          src: "./node_modules/bootstrap/dist/js/bootstrap.min.*",
+          dest: "./assets/dist/vendor",
+        },
+        {
+          src: "./node_modules/jquery/dist/jquery.slim.min.*",
+          dest: "./assets/dist/vendor",
+        },
+        {
+          src: "./node_modules/list.js/dist/list.min.*",
+          dest: "./assets/dist/vendor",
+        },
+        {
+          src: "./node_modules/select2/dist/css/select2.min.css",
+          dest: "./assets/dist/vendor",
+        },
+        {
+          src: "./node_modules/select2/dist/js/select2.min.js",
+          dest: "./assets/dist/vendor",
+        },
+      ],
+      hook: "writeBundle",
+    }),
+  ],
 };
 
 export default config;
