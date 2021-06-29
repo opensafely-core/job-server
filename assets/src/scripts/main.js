d86abc5bb4d30e159b04578b35f6d7ee7f31ff47
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-06-29 15:08:03 +0100

    Update Plausible Analytics script

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,2 +1,12 @@
 import "vite/dynamic-import-polyfill";
 import "../styles/main.scss";
+
+if (document.location.hostname === "jobs.opensafely.org") {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.setAttribute("data-domain", "jobs.opensafely.org");
+  script.id = "plausible";
+  script.src = "https://plausible.io/js/plausible.compat.js";
+
+  document.head.appendChild(script);
+}
