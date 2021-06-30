7b7d2d0def8fa11b577c8168816d71209f7b1a33
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-06-30 13:58:34 +0100

    Add basic query for file list

--- assets/src/scripts/results-viewer/App.jsx
+++ assets/src/scripts/results-viewer/App.jsx
@@ -1,13 +1,40 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+function FileList() {
+  const { isLoading, isError, data, error } = useQuery(
+    "FILE_LIST",
+    () => fetch("/"),
+    {
+      initialData: window.props,
+      staleTime: Infinity,
+    }
+  );
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return <h1>{data.name}</h1>;
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hello world</h1>
+      <FileList />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
