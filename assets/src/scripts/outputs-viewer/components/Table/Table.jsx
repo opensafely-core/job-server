bd42610283b72e44150802c13b7240ffe8f2ca62
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-07-15 15:19:59 +0100

    Replace react-table
    Saving 55.67 kB

--- assets/src/scripts/outputs-viewer/components/Table/Table.jsx
+++ assets/src/scripts/outputs-viewer/components/Table/Table.jsx
@@ -1,51 +1,40 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { readString } from "react-papaparse";
-import { useTable } from "react-table";
+
+const TableCell = ({ cell }) => <td>{cell}</td>;
+
+const TableRow = ({ row }) => (
+  <tr>
+    {row.map((cell) => (
+      <TableCell key={row + cell} cell={cell} />
+    ))}
+  </tr>
+);
 
 function Table({ data }) {
-  const memoData = React.useMemo(
-    () =>
-      readString(data, {
-        header: true,
-        complete: (results) => results,
-      }).data,
-    [data]
-  );
-
-  const columns = React.useMemo(
-    () =>
-      readString(data, {
-        header: true,
-        complete: (results) => results,
-      }).meta.fields.map((header) => ({
-        accessor: header,
-      })),
-    [data]
-  );
-
-  const { getTableProps, getTableBodyProps, rows, prepareRow } = useTable({
-    columns,
-    data: memoData,
-  });
+  const jsonData = readString(data, {
+    complete: (results) => results,
+  }).data;
 
   return (
-    <table {...getTableProps()}>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-              ))}
-            </tr>
-          );
-        })}
+    <table>
+      <tbody>
+        {jsonData.map((row) => (
+          <TableRow key={row.join(",")} row={row} />
+        ))}
       </tbody>
     </table>
   );
 }
+
+TableCell.propTypes = {
+  cell: PropTypes.string.isRequired,
+};
+
+TableRow.propTypes = {
+  row: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 Table.propTypes = {
   data: PropTypes.string.isRequired,
