688f3078b667c80091bcf6dbf888bdc4a99ac8ef
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-07-15 14:19:10 +0100

    Simplify file sorter function

--- assets/src/scripts/outputs-viewer/components/FileList/FileList.jsx
+++ assets/src/scripts/outputs-viewer/components/FileList/FileList.jsx
@@ -24,14 +24,8 @@ function FileList({ apiUrl, setFileUrl }) {
       const nameA = a.name.toUpperCase();
       const nameB = b.name.toUpperCase();
 
-      if (nameA < nameB) {
-        return -1;
-      }
-
-      if (nameA > nameB) {
-        return 1;
-      }
-
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
       return 0;
     }),
   ];
