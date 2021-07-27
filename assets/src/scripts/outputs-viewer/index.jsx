9aa9ebb619ce7634c6c1fb37e0062f2fdf76af23
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-07-27 14:20:55 +0100

    Track React errors using Sentry

--- assets/src/scripts/outputs-viewer/index.jsx
+++ assets/src/scripts/outputs-viewer/index.jsx
@@ -1,6 +1,17 @@
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
+if (
+  process.env.NODE_ENV === "production" &&
+  import.meta.env.VITE_REACT_SENTRY
+) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_REACT_SENTRY,
+    tracesSampleRate: 1.0,
+  });
+}
 
 const element = document.getElementById("outputsSPA");
 
