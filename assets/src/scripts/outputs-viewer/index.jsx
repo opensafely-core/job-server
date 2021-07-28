0da7284f0cee3ac42941a396e7daddda3cf2d540
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-07-28 14:00:20 +0100

    Use env var in Vite build for DSN

--- assets/src/scripts/outputs-viewer/index.jsx
+++ assets/src/scripts/outputs-viewer/index.jsx
@@ -3,12 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-if (
-  process.env.NODE_ENV === "production" &&
-  document.location.hostname === "jobs.opensafely.org"
-) {
+if (document.location.hostname === "jobs.opensafely.org") {
   Sentry.init({
-    dsn: "https://adf34e0d9a5445fea760b43cf10ad45b@o173701.ingest.sentry.io/5881265",
+    dsn: import.meta.env.VITE_REACT_SENTRY,
     tracesSampleRate: 1.0,
   });
 }
