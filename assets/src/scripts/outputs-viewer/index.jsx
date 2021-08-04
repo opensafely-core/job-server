f3059647b1e998522c07200cabf44222dde6aade
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-08-04 15:59:12 +0100

    Remove unused polyfill

--- assets/src/scripts/outputs-viewer/index.jsx
+++ assets/src/scripts/outputs-viewer/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import useStore from "./stores/use-store";
-import "vite/dynamic-import-polyfill";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
