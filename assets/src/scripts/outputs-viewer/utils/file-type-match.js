2643c733855d529623d4a076a66a901c2aad7bd5
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-08-05 12:06:39 +0100

    fix: compare lowercase file name

--- assets/src/scripts/outputs-viewer/utils/file-type-match.js
+++ assets/src/scripts/outputs-viewer/utils/file-type-match.js
@@ -1,13 +1,13 @@
-export const isCsv = (file) => file.name.match(/.*\.(?:csv)$/i);
+export const isCsv = (file) => file.name.toLowerCase().match(/.*\.(?:csv)$/i);
 
-export const isHtml = (file) => file.name.match(/.*\.(?:html)$/i);
+export const isHtml = (file) => file.name.toLowerCase().match(/.*\.(?:html)$/i);
 
 export const isImg = (file) =>
-  file.name.match(/.*\.(?:gif|jpg|jpeg|png|svg)$/i);
+  file.name.toLowerCase().match(/.*\.(?:gif|jpg|jpeg|png|svg)$/i);
 
-export const isTxt = (file) => file.name.match(/.*\.(?:txt)$/i);
+export const isTxt = (file) => file.name.toLowerCase().match(/.*\.(?:txt)$/i);
 
-export const isJson = (file) => file.name.match(/.*\.(?:json)$/i);
+export const isJson = (file) => file.name.toLowerCase().match(/.*\.(?:json)$/i);
 
 export const canDisplay = (file) =>
   isCsv(file) || isHtml(file) || isImg(file) || isTxt(file) || isJson(file);
