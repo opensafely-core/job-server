f982e92ee1316fe3d972352c1398517b91518ad1
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-08-26 10:54:35 +0100

    Add organisations to the main nav

--- jobserver/context_processors.py
+++ jobserver/context_processors.py
@@ -42,7 +42,12 @@ def nav(request):
 
     options = [
         {
-            "name": "Event Log",
+            "name": "Organisations",
+            "is_active": _active(reverse("org-list")),
+            "url": reverse("org-list"),
+        },
+        {
+            "name": "Event log",
             "is_active": _active(reverse("job-list")),
             "url": reverse("job-list"),
         },
