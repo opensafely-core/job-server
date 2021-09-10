0bda83d6f8e653a082271dcdb7c5d6384906502b
George Hickman <george@ghickman.co.uk>
2021-09-10 11:32:47 +0100

    Notify slack when a new user registers

--- jobserver/pipeline.py
+++ jobserver/pipeline.py
@@ -1,3 +1,22 @@
+from django.conf import settings
+from furl import furl
+
+from services.slack import client as slack_client
+
+
+def notify_on_new_user(user, is_new, *args, **kwargs):
+    if not is_new:
+        return
+
+    f = furl(settings.BASE_URL)
+    f.path = user.get_staff_url()
+
+    slack_client.chat_postMessage(
+        channel="job-server-registrations",
+        text=f"New user ({user.username}) registered: {f.url}",
+    )
+
+
 def set_notifications_email(user, *args, **kwargs):
     """
     Set a User's notifications_email if it's not already set
--- tests/unit/jobserver/test_pipeline.py
+++ tests/unit/jobserver/test_pipeline.py
@@ -1,6 +1,32 @@
-from jobserver.pipeline import set_notifications_email
+from jobserver.pipeline import notify_on_new_user, set_notifications_email
 
 from ...factories import UserFactory
+
+
+def test_notify_on_new_user_with_existing_user(mocker):
+    user = UserFactory()
+
+    mock = mocker.patch("jobserver.pipeline.slack_client", autospec=True)
+
+    notify_on_new_user(user, is_new=False)
+
+    mock.chat_postMessage.assert_not_called()
+
+
+def test_notify_on_new_user_with_new_user(mocker):
+    user = UserFactory()
+
+    mock = mocker.patch("jobserver.pipeline.slack_client", autospec=True)
+
+    notify_on_new_user(user, is_new=True)
+
+    mock.chat_postMessage.assert_called_once()
+
+    url = f"http://localhost:8000{user.get_staff_url()}"
+    mock.chat_postMessage.assert_called_with(
+        channel="job-server-registrations",
+        text=f"New user ({user.username}) registered: {url}",
+    )
 
 
 def test_set_notifications_email_already_set():
