e337cc9e9ca004162762b55dd64b078902e139eb
George Hickman <george@ghickman.co.uk>
2021-09-20 16:59:17 +0100

    add some very basic tests to fix error

--- tests/unit/applications/test_views.py
+++ tests/unit/applications/test_views.py
@@ -1,13 +1,28 @@
-import pytest
+from django.urls import reverse
 
-from applications.views import application
+from applications.models import Application
+from applications.views import terms
 
-from ...factories import UserFactory
 
-
-@pytest.mark.django_db
-def test_get_application(rf):
+def test_terms_get_success(rf):
     request = rf.get("/")
-    request.user = UserFactory()
-    response = application(request)
+
+    response = terms(request)
+
     assert response.status_code == 200
+
+
+def test_terms_post_success(rf):
+    request = rf.post("/")
+
+    assert Application.objects.count() == 0
+
+    response = terms(request)
+
+    assert response.status_code == 302
+
+    application = Application.objects.first()
+    expected_url = reverse(
+        "applications:page", kwargs={"pk": application.pk, "page_num": 1}
+    )
+    assert response.url == expected_url
