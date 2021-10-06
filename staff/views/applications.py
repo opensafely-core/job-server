edf39c556c4ded0f0557971e86361a59555dec10
Peter Inglesby <peter.inglesby@gmail.com>
2021-10-06 14:26:02 +0100

    Dynamically generate fields for staff application page

--- staff/views/applications.py
+++ staff/views/applications.py
@@ -2,7 +2,9 @@ from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView
 
+from applications.form_specs import form_specs
 from applications.models import Application
+from applications.wizard import Wizard
 from jobserver.authorization import CoreDeveloper
 from jobserver.authorization.decorators import require_role
 
@@ -13,8 +15,36 @@ class ApplicationDetail(DetailView):
     template_name = "staff/application_detail.html"
 
     def get_context_data(self, **kwargs):
+        wizard = Wizard(self.object, form_specs)
+        pages = []
+
+        for page in wizard.get_pages():
+            fieldsets = []
+            for fieldset in page.form_spec.fieldsets:
+                fields = []
+                for field in fieldset.fields:
+                    fields.append(
+                        {
+                            "label": field.label,
+                            "value": getattr(page.instance, field.name),
+                        }
+                    )
+                fieldsets.append(
+                    {
+                        "label": fieldset.label,
+                        "fields": fields,
+                    }
+                )
+            pages.append(
+                {
+                    "title": page.title,
+                    "fieldsets": fieldsets,
+                }
+            )
+
         return super().get_context_data(**kwargs) | {
             "researchers": self.object.researcher_registrations.order_by("created_at"),
+            "pages": pages,
         }
 
 
