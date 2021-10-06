79601dff5f01985aaf20ea060c37448d18d00be4
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-10-06 10:28:46 +0100

    Revert "Remove institutional_rec_reference from sponsor-details"
    This reverts commit 927bddb9bda25462c8105138af096fc28b27a4b1.

--- applications/form_specs.py
+++ applications/form_specs.py
@@ -256,6 +256,15 @@ form_specs = [
         footer=snippet("8-footer"),
         fieldsets=[
             Fieldset(
+                label="Service evaluation or an audit",
+                fields=[
+                    Field(
+                        name="institutional_rec_reference",
+                        label="Institutional REC reference",
+                    ),
+                ],
+            ),
+            Fieldset(
                 label="Sponsor information",
                 fields=[
                     Field(
