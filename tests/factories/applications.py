9f8b042bb51ac5cc325a1caa6b0cc2df4ac5738a
George Hickman <george@ghickman.co.uk>
2021-10-08 11:37:32 +0100

    Build out all page model factories
    For fully valid applications we need all the page models to have values.

--- tests/factories/applications.py
+++ tests/factories/applications.py
@@ -1,4 +1,5 @@
 import factory
+import factory.fuzzy
 from pytz import utc
 
 from applications.models import (
@@ -31,116 +32,159 @@ class ApplicationFactory(factory.django.DjangoModelFactory):
     project = factory.SubFactory("tests.factories.ProjectFactory")
 
 
-class ContactDetailsPageFactory(factory.django.DjangoModelFactory):
+class AbstractPageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        abstract = True
+
+    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    reviewed_by = factory.SubFactory("tests.factories.UserFactory")
+
+    notes = factory.Sequence(lambda n: f"note {n}")
+    is_approved = factory.fuzzy.FuzzyChoice([True, False])
+
+    last_reviewed_at = factory.Faker("date_time", tzinfo=utc)
+
+    created_at = factory.Faker("date_time", tzinfo=utc)
+    updated_at = factory.Faker("date_time", tzinfo=utc)
+
+
+class ContactDetailsPageFactory(AbstractPageFactory):
     class Meta:
         model = ContactDetailsPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    full_name = factory.Sequence(lambda n: f"name {n}")
+    email = factory.Sequence(lambda n: f"name{n}@example.com")
+    telephone = factory.Faker("phone_number")
+    job_title = factory.Faker("job")
+    team_name = factory.Sequence(lambda n: f"team {n}")
+    organisation = factory.Sequence(lambda n: f"org {n}")
 
 
-class StudyInformationPageFactory(factory.django.DjangoModelFactory):
+class StudyInformationPageFactory(AbstractPageFactory):
     class Meta:
         model = StudyInformationPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    study_name = factory.Sequence(lambda n: f"study {n}")
+    study_purpose = factory.Sequence(lambda n: f"purpose {n}")
 
 
-class StudyPurposePageFactory(factory.django.DjangoModelFactory):
+class StudyPurposePageFactory(AbstractPageFactory):
     class Meta:
         model = StudyPurposePage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    description = factory.Sequence(lambda n: f"description {n}")
+    author_name = factory.Sequence(lambda n: f"author name {n}")
+    author_email = factory.Sequence(lambda n: f"name{n}@example.com")
+    author_organisation = factory.Sequence(lambda n: f"author org {n}")
 
 
-class StudyDataPageFactory(factory.django.DjangoModelFactory):
+class StudyDataPageFactory(AbstractPageFactory):
     class Meta:
         model = StudyDataPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    data_meets_purpose = factory.Sequence(lambda n: f"data meets purpose {n}")
+    need_record_level_data = factory.fuzzy.FuzzyChoice([True, False])
 
 
-class RecordLevelDataPageFactory(factory.django.DjangoModelFactory):
+class RecordLevelDataPageFactory(AbstractPageFactory):
     class Meta:
         model = RecordLevelDataPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    record_level_data_reasons = factory.Sequence(
+        lambda n: f"record level data reasons {n}"
+    )
 
 
-class TypeOfStudyPageFactory(factory.django.DjangoModelFactory):
+class TypeOfStudyPageFactory(AbstractPageFactory):
     class Meta:
         model = TypeOfStudyPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    is_study_research = factory.fuzzy.FuzzyChoice([True, False])
+    is_study_service_evaluation = factory.fuzzy.FuzzyChoice([True, False])
+    is_study_audit = factory.fuzzy.FuzzyChoice([True, False])
 
 
-class ReferencesPageFactory(factory.django.DjangoModelFactory):
+class ReferencesPageFactory(AbstractPageFactory):
     class Meta:
         model = ReferencesPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    hra_ires_id = factory.Sequence(lambda n: f"HRA IRES ID {n}")
+    hra_rec_reference = factory.Sequence(lambda n: f"HRA REC {n}")
+    institutional_rec_reference = factory.Sequence(lambda n: f"Institutional REC {n}")
 
 
-class SponsorDetailsPageFactory(factory.django.DjangoModelFactory):
+class SponsorDetailsPageFactory(AbstractPageFactory):
     class Meta:
         model = SponsorDetailsPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    sponsor_name = factory.Sequence(lambda n: f"Sponsor {n}")
+    sponsor_email = factory.Sequence(lambda n: f"sponsor{n}@example.com")
+    sponsor_job_role = factory.Faker("job")
+    institutional_rec_reference = factory.Sequence(lambda n: f"Institutional REC {n}")
 
 
-class CmoPriorityListPageFactory(factory.django.DjangoModelFactory):
+class CmoPriorityListPageFactory(AbstractPageFactory):
     class Meta:
         model = CmoPriorityListPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    is_on_cmo_priority_list = factory.fuzzy.FuzzyChoice([True, False])
 
 
-class LegalBasisPageFactory(factory.django.DjangoModelFactory):
+class LegalBasisPageFactory(AbstractPageFactory):
     class Meta:
         model = LegalBasisPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    legal_basis_for_accessing_data_under_dpa = factory.Sequence(
+        lambda n: f"legal basis {n}"
+    )
+    how_is_duty_of_confidentiality_satisfied = factory.Sequence(
+        lambda n: f"confidentiality {n}"
+    )
 
 
-class StudyFundingPageFactory(factory.django.DjangoModelFactory):
+class StudyFundingPageFactory(AbstractPageFactory):
     class Meta:
         model = StudyFundingPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    funding_details = factory.Sequence(lambda n: f"funding details {n}")
 
 
-class TeamDetailsPageFactory(factory.django.DjangoModelFactory):
+class TeamDetailsPageFactory(AbstractPageFactory):
     class Meta:
         model = TeamDetailsPage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    team_details = factory.Sequence(lambda n: f"team details {n}")
 
 
-class PreviousEhrExperiencePageFactory(factory.django.DjangoModelFactory):
+class PreviousEhrExperiencePageFactory(AbstractPageFactory):
     class Meta:
         model = PreviousEhrExperiencePage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    previous_experience_with_ehr = factory.Sequence(
+        lambda n: f"previous EHR experience {n}"
+    )
 
 
-class SoftwareDevelopmentExperiencePageFactory(factory.django.DjangoModelFactory):
+class SoftwareDevelopmentExperiencePageFactory(AbstractPageFactory):
     class Meta:
         model = SoftwareDevelopmentExperiencePage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    evidence_of_coding = factory.Sequence(lambda n: f"evidence of coding {n}")
+    all_applicants_completed_getting_started = factory.fuzzy.FuzzyChoice([True, False])
 
 
-class SharingCodePageFactory(factory.django.DjangoModelFactory):
+class SharingCodePageFactory(AbstractPageFactory):
     class Meta:
         model = SharingCodePage
 
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
+    evidence_of_sharing_in_public_domain_before = factory.Sequence(
+        lambda n: f"sharing in public domain {n}"
+    )
 
 
-class ResearcherDetailsPageFactory(factory.django.DjangoModelFactory):
+class ResearcherDetailsPageFactory(AbstractPageFactory):
     class Meta:
         model = ResearcherDetailsPage
-
-    application = factory.SubFactory("tests.factories.ApplicationFactory")
 
 
 class ResearcherRegistrationFactory(factory.django.DjangoModelFactory):
@@ -148,6 +192,5 @@ class ResearcherRegistrationFactory(factory.django.DjangoModelFactory):
         model = ResearcherRegistration
 
     application = factory.SubFactory("tests.factories.ApplicationFactory")
-
     has_taken_safe_researcher_training = True
     training_passed_at = factory.Faker("date_time", tzinfo=utc)
