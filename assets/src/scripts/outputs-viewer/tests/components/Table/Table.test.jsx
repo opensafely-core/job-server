0141839988a428b96597be8e6de13566339bcc14
bloodearnest <simon.davy@thedatalab.org>
2022-01-20 16:09:23 +0000

    fix: remove the 5000 line limit on displaying CSVs
    We've already loaded the entire JSON, and we only display the first 1000
    rows, so this is no more resource intensive, and more user friendly

--- assets/src/scripts/outputs-viewer/tests/components/Table/Table.test.jsx
+++ assets/src/scripts/outputs-viewer/tests/components/Table/Table.test.jsx
@@ -4,9 +4,6 @@ import { csvExample, csvFile } from "../../helpers/files";
 import { render, screen } from "../../test-utils";
 
 describe("<Table />", () => {
-  const fiveThousandRows = `${Array(5000).fill(`a
-  `)}`.trimEnd();
-
   const twoThousandRows = `${Array(2000).fill(`b
   `)}`.trimEnd();
 
@@ -16,17 +13,6 @@ describe("<Table />", () => {
     const cells = screen.getAllByRole("cell");
     expect(cells[0].textContent).toEqual("gradually");
     expect(cells[cells.length - 1].textContent).toEqual("select");
-  });
-
-  it("displays the <NoPreview /> component for CSVs with more than 5000 rows", async () => {
-    render(<Table data={fiveThousandRows} />, {}, { file: csvFile });
-
-    expect(screen.getByRole("link").textContent).toBe(
-      "Open file in a new tab ↗"
-    );
-    expect(screen.getByRole("link").href).toEqual(
-      `http://localhost${csvFile.url}`
-    );
   });
 
   it("displays the first 1000 rows for CSVs with more than 1000 and fewer than 5000 rows", async () => {
