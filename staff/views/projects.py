9ec7e4e2f20d26a4d91b69275fd3bbe73f39cfe6
George Hickman <george@ghickman.co.uk>
2022-06-16 16:20:04 +0100

    Order Projects by number then name in the Staff Area
    Not all Projects have a number yet so we can't rely on that for ordering
    yet.

--- staff/views/projects.py
+++ staff/views/projects.py
@@ -186,7 +186,7 @@ class ProjectLinkApplication(UpdateView):
 
 @method_decorator(require_role(CoreDeveloper), name="dispatch")
 class ProjectList(ListView):
-    queryset = Project.objects.select_related("org").order_by(Lower("name"))
+    queryset = Project.objects.select_related("org").order_by("number", Lower("name"))
     template_name = "staff/project_list.html"
 
     def get_context_data(self, **kwargs):
