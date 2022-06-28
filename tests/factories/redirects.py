3ee718718f504558bb5c11a8aaf98f6fae94496f
George Hickman <george@ghickman.co.uk>
2022-06-28 14:43:42 +0100

    Add redirections

---
+++ tests/factories/redirects.py
@@ -0,0 +1,10 @@
+import factory
+
+from redirects.models import Redirect
+
+
+class RedirectFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Redirect
+
+    created_by = factory.SubFactory("tests.factories.UserFactory")
