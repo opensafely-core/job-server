397ca578652225f614cf79171807c36cf22b2b57
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-08-03 11:35:16 +0100

    Tweaks and fixes for job_detail and config

--- assets/src/scripts/tw.js
+++ assets/src/scripts/tw.js
@@ -1,61 +1,56 @@
-import Alpine from "alpinejs/packages/csp/dist/module.esm";
+/* eslint-disable no-return-assign */
 import Screen from "@alpine-collective/toolkit-screen/dist/module.esm";
+import Alpine from "alpinejs/packages/csp/dist/module.esm";
 import "../styles/tw.css";
 
 Alpine.plugin(Screen);
 
 document.addEventListener("alpine:init", () => {
-  Alpine.data("header", function () {
-    return {
-      get largerThanMd() {
-        return this.$screen("md");
-      },
+  Alpine.data("header", () => ({
+    get largerThanMd() {
+      return this.$screen("md");
+    },
 
-      showMainNav: false,
-      get isMainNavOpen() {
-        if (this.largerThanMd) {
-          return true;
-        }
-        return this.showMainNav;
-      },
-      clickMainNav() {
-        if (this.largerThanMd) {
-          this.showMainNav = true;
-        } else {
-          return (this.showMainNav = !this.showMainNav);
-        }
-      },
-      closeMainNav() {
-        if (this.largerThanMd) {
-          return (this.showMainNav = true);
-        } else {
-          this.showMainNav = false;
-        }
-      },
+    showMainNav: false,
+    get isMainNavOpen() {
+      if (this.largerThanMd) {
+        return true;
+      }
+      return this.showMainNav;
+    },
+    clickMainNav() {
+      if (this.largerThanMd) {
+        this.showMainNav = true;
+      }
+      return (this.showMainNav = !this.showMainNav);
+    },
+    closeMainNav() {
+      if (this.largerThanMd) {
+        return (this.showMainNav = true);
+      }
+      return (this.showMainNav = false);
+    },
 
-      showUserNav: false,
-      get isUserNavOpen() {
-        if (!this.largerThanMd) {
-          return true;
-        }
-        return this.showUserNav;
-      },
-      clickUserNav() {
-        if (this.largerThanMd) {
-          return (this.showUserNav = !this.showUserNav);
-        } else {
-          this.showUserNav = true;
-        }
-      },
-      closeUserNav() {
-        if (this.largerThanMd) {
-          return (this.showUserNav = false);
-        } else {
-          this.showUserNav = this.showUserNav;
-        }
-      },
-    };
-  });
+    showUserNav: false,
+    get isUserNavOpen() {
+      if (!this.largerThanMd) {
+        return true;
+      }
+      return this.showUserNav;
+    },
+    clickUserNav() {
+      if (this.largerThanMd) {
+        return (this.showUserNav = !this.showUserNav);
+      }
+      return (this.showUserNav = true);
+    },
+    closeUserNav() {
+      if (this.largerThanMd) {
+        return (this.showUserNav = false);
+      }
+      return null;
+    },
+  }));
 });
 
 window.Alpine = Alpine;
