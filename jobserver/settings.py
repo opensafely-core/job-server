34d0cf4f862da03131cc9c181ef3119edbeae4d2
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-08-04 15:07:39 +0100

    Update CSP to include nonce in script-src

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -283,18 +283,21 @@ CSP_SCRIPT_SRC = CSP_SCRIPT_SRC_ELEM = ["'self'", "https://plausible.io"]
 CSP_STYLE_SRC = CSP_STYLE_SRC_ELEM = ["'self'"]
 
 # which directives to set a nonce for
-CSP_INCLUDE_NONCE_IN = ["script-src-elem"]
+CSP_INCLUDE_NONCE_IN = ["script-src", "script-src-elem"]
 
 # configure django-csp to work with Vite when using it in dev mode
 if DJANGO_VITE_DEV_MODE:
-    CSP_CONNECT_SRC = ["'self'", "ws://localhost:3000/static/"]
-    CSP_FONT_SRC = ["'self'", "data:"]
+    CSP_CONNECT_SRC = ["'self'", "ws://localhost:3000/static/", "https://plausible.io"]
+    CSP_FONT_SRC = ["'self'", "http://localhost:3000"]
     CSP_SCRIPT_SRC = CSP_SCRIPT_SRC_ELEM = [
         "'self'",
         "https://plausible.io",
         "http://localhost:3000",
     ]
-    CSP_STYLE_SRC = CSP_STYLE_SRC_ELEM = ["'self'", "'unsafe-inline'"]
+    CSP_STYLE_SRC = CSP_STYLE_SRC_ELEM = [
+        "'self'",
+        "'unsafe-inline'",
+    ]
 
 
 # THIRD PARTY SETTINGS
