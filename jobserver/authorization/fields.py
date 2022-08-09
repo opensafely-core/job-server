0678fb7a7befa8b99ca0425550b65beeec5d4e7a
George Hickman <george@ghickman.co.uk>
2022-08-09 09:53:50 +0100

    skip parsing role paths when they're any kind of falsey value
    We have a default of an empty list for this field so we can skip parsing
    that now too.

--- jobserver/authorization/fields.py
+++ jobserver/authorization/fields.py
@@ -22,7 +22,7 @@ class RolesField(models.JSONField):
     def from_db_value(self, value, expression, connection):
         paths = super().from_db_value(value, expression, connection)
 
-        if paths is None:
+        if not paths:
             return paths
 
         return parse_roles(paths)
