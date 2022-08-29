03ef7ea757dbd79b989e6b3f58bc59ca1ac77194
bloodearnest <simon.davy@thedatalab.org>
2022-08-29 16:50:12 +0100

    chore: fix the local release-hatch script after local testing

--- scripts/local-release-hatch.sh
+++ scripts/local-release-hatch.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 
 RELEASE_HATCH_REPO=${RELEASE_HATCH_REPO:-./release-hatch}
 RELEASE_HATCH_URL=${RELEASE_HATCH_URL:-http://localhost:8001}
-WORKSPACE_DIR=${WORKSPACE_DIR:-./workspaces}
+WORKSPACE_DIR=${WORKSPACE_DIR:-$PWD/workspaces}
 
 mkdir -p "$WORKSPACE_DIR"
 
@@ -12,7 +12,9 @@ if test -d "$RELEASE_HATCH_REPO"; then
 else
     git clone https://github.com/opensafely-core/release-hatch "$RELEASE_HATCH_REPO"
 fi
-env -C "$RELEASE_HATCH_REPO" VIRTUAL_ENV="$RELEASE_HATCH_REPO/.venv" just devenv
+
+# set VIRTUAL_ENV explicitly so we don't use job-server's venv
+env -C "$RELEASE_HATCH_REPO" VIRTUAL_ENV=".venv" just devenv
 
 JOB_SERVER_TOKEN="$("$VIRTUAL_ENV/bin/python" manage.py shell << EOF
 from jobserver.models.backends import Backend
@@ -27,14 +29,17 @@ print(backend.auth_token)
 EOF
 )"
 
-JOB_SERVER_ENDPOINT="$("$VIRTUAL_ENV/bin/python" manage.py shell -c 'from django.conf import settings; print(settings.BASE_URL)')"
+JOB_SERVER="$("$VIRTUAL_ENV/bin/python" manage.py shell -c 'from django.conf import settings; print(settings.BASE_URL)')"
 
 cat > "$RELEASE_HATCH_REPO/.env" << EOF
 JOB_SERVER_TOKEN=$JOB_SERVER_TOKEN
-JOB_SERVER_ENDPOINT=$JOB_SERVER_ENDPOINT
+JOB_SERVER_ENDPOINT=$JOB_SERVER/api/v2
+SPA_ORIGIN=$JOB_SERVER
 RELEASE_HOST=$RELEASE_HATCH_URL
 WORKSPACES=$WORKSPACE_DIR
+LOG_LEVEL=debug
 EOF
 cat "$RELEASE_HATCH_REPO/.env"
 
-env -C "$RELEASE_HATCH_REPO" VIRTUAL_ENV="$RELEASE_HATCH_REPO/.venv" just run
+# set VIRTUAL_ENV explicitly so we don't use job-server's venv
+env -C "$RELEASE_HATCH_REPO" VIRTUAL_ENV=".venv" just run
