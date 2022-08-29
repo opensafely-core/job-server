2c0f96e9367256c11a6394b1936db43879f182e9
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-08-29 11:06:47 +0100

    Build a prototype releases UI (#2055)
    Add the prototype file-selection Release UI
    
    We have merged this with a squash merge to make it easy to revert the change after doing some demoing, below are the original commit messages:
    * Add React Bootstrap as a dependency
    * Add new data to outputs SPA dataset
    * Update outputs SPA dataset
    * Use React Bootstrap components
    * Set up adding files to review
    * Set up Zustand as a file store
    * Add example submit modal
    * Show submission data on modal
    * Update JSON output and use URL to compare
    * Remove shortName
    * Fixes for non-review mode
    * Add form submit for modal
    * Fix linting errors
    * Ignore JS tests
    * Improve store usage
    * Fix linting errors
    * Change window location base on headers
    * Display metadata for ReleaseFiles
    * Show Release metadata above the SPA
    
    Co-authored-by: George Hickman <george@ghickman.co.uk>

--- assets/src/scripts/outputs-viewer/components/FileList/FileList.jsx
+++ assets/src/scripts/outputs-viewer/components/FileList/FileList.jsx
@@ -1,71 +1,23 @@
 import PropTypes from "prop-types";
-import React, { createRef, useEffect, useRef, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { FixedSizeList } from "react-window";
+import React, { createRef, useState } from "react";
+import { Card, FormCheck, ListGroup } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 import useFileList from "../../hooks/use-file-list";
-import useWindowSize from "../../hooks/use-window-size";
-import prettyFileSize from "../../utils/pretty-file-size";
-import { datasetProps } from "../../utils/props";
+import useFileStore from "../../stores/use-file-store";
 import Filter from "./Filter";
 
-function FileList({
-  authToken,
-  filesUrl,
-  listVisible,
-  setListVisible,
-  setSelectedFile,
-}) {
+function FileList({ isReviewEdit, listVisible, setSelectedFile }) {
   const [files, setFiles] = useState([]);
-  const [listHeight, setListHeight] = useState(0);
-  const [fileIndex, setFileIndex] = useState(null);
-
-  const { data, isError, isLoading } = useFileList({ authToken, filesUrl });
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const windowSize = useWindowSize();
-
-  const listEl = useRef(null);
   const listRef = createRef();
+  const location = useLocation();
+  const { isFileChecked } = useFileStore((state) => ({
+    isFileChecked: state.isFileChecked,
+    checkedFiles: state.checkedFiles, // Update when checkedFiles changes
+  }));
 
-  useEffect(() => {
-    const largeViewport = window.innerWidth > 991;
-    const hasScrollbarX =
-      listEl.current?.clientWidth < listEl.current?.scrollWidth;
-
-    const fileListHeight =
-      // If the viewport height is taller than 600px
-      // Use the viewport height
-      // Otherwise use 600px
-      (window.innerHeight > 600 ? window.innerHeight : 600) -
-      // if the list exists
-      // minus the height from the top of the list
-      // else minus zero
-      (listEl.current?.getBoundingClientRect().top || 0) -
-      // minus 30px for spacing at the bottom
-      30 -
-      // if there are horizontal scrollbars
-      // minus 17px for the scrollbar (magic number)
-      (hasScrollbarX ? 17 : 0);
-
-    if (largeViewport) {
-      setListVisible(true);
-      return setListHeight(fileListHeight);
-    }
-
-    return setListHeight(fileListHeight);
-  }, [files, listVisible, setListVisible, windowSize]);
-
-  useEffect(() => {
-    const selectedItem = files.findIndex(
-      (file) => `/${file.name}` === location.pathname
-    );
-
-    if (files[selectedItem]) {
-      setFileIndex(selectedItem);
-      setSelectedFile(files[selectedItem]);
-    }
-  }, [data, files, location, setSelectedFile]);
+  const { data, isError, isLoading } = useFileList({
+    setSelectedFile,
+  });
 
   if (isLoading) {
     return (
@@ -87,61 +39,33 @@ function FileList({
     );
   }
 
-  const selectFile = ({ e, item }) => {
-    e.preventDefault();
-
-    const itemName = `/${item.name}`;
-
-    // Don't push a state change if clicking on a new file
-    if (
-      itemName === location.pathname ||
-      itemName === location?.location?.pathname
-    ) {
-      return null;
-    }
-
-    navigate(itemName);
-    return setSelectedFile(item);
-  };
-
   return (
     <div className={`sidebar ${listVisible ? "d-block" : "d-none"}`}>
       <Filter files={data} listRef={listRef} setFiles={setFiles} />{" "}
-      <div className="card pt-2">
-        <FixedSizeList
-          ref={listRef}
-          className="list"
-          height={listHeight}
-          innerElementType="ul"
-          itemCount={files.length}
-          itemSize={25}
-          outerRef={listEl}
-          width="100%"
-        >
-          {({ index, style }) => (
-            <li
-              className={`list-item ${
-                fileIndex === index && "list-item--selected"
-              }`}
-              style={style}
+      <Card className="" style={{ maxHeight: "1000px", overflowY: "scroll" }}>
+        <ListGroup variant="flush">
+          {files.map((file) => (
+            <ListGroup.Item
+              key={file.id}
+              action
+              active={location.pathname.substring(1) === file.name}
+              as={Link}
+              onClick={() => setSelectedFile(file)}
+              to={file.name}
             >
-              {fileIndex === index ? (
-                <span>{files[index].shortName}</span>
+              {isReviewEdit ? (
+                <FormCheck
+                  checked={isFileChecked(file)}
+                  label={file.shortName}
+                  readOnly
+                />
               ) : (
-                <a
-                  className="list-item__link"
-                  disabled={`/${files[index].name}` === location.pathname}
-                  href={files[index].url}
-                  onClick={(e) => selectFile({ e, item: files[index] })}
-                  title={`File size: ${prettyFileSize(files[index].size)}`}
-                >
-                  {files[index].shortName}
-                </a>
+                file.shortName
               )}
-            </li>
-          )}
-        </FixedSizeList>
-      </div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
     </div>
   );
 }
@@ -149,9 +73,11 @@ function FileList({
 export default FileList;
 
 FileList.propTypes = {
-  authToken: datasetProps.authToken.isRequired,
-  filesUrl: datasetProps.filesUrl.isRequired,
+  isReviewEdit: PropTypes.bool,
   listVisible: PropTypes.bool.isRequired,
-  setListVisible: PropTypes.func.isRequired,
   setSelectedFile: PropTypes.func.isRequired,
+};
+
+FileList.defaultProps = {
+  isReviewEdit: false,
 };
---
+++ assets/src/scripts/outputs-viewer/stores/use-file-store.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-return-assign */
+import { mountStoreDevtool } from "simple-zustand-devtools";
+import create from "zustand";
+
+const useFileStore = create((set, get) => ({
+  checkedFiles: [],
+
+  isFileChecked: (file) =>
+    get().checkedFiles.some((checked) => checked.url === file.url),
+  addCheckedFile: (file) => {
+    if (get().isFileChecked(file)) {
+      return null;
+    }
+
+    return set((state) => ({
+      checkedFiles: [
+        {
+          name: file.name,
+          url: file.url,
+          date: file.date,
+          sha256: file.sha256,
+          size: file.size,
+          metadata: {},
+        },
+        ...state.checkedFiles,
+      ],
+    }));
+  },
+  removeCheckedFile: (file) =>
+    set((state) => ({
+      checkedFiles: [...state.checkedFiles].filter(
+        (checked) => checked.url !== file.url
+      ),
+    })),
+
+  setFileMeta: (file, field, text) => {
+    const fileIndex = get().checkedFiles.findIndex(
+      (checked) => checked.url === file.url
+    );
+    Object.assign(get().checkedFiles[fileIndex], {
+      metadata: {
+        ...get().checkedFiles[fileIndex].metadata,
+        [field]: text,
+      },
+    });
+
+    set((state) => ({ checkedFiles: [...state.checkedFiles] }));
+  },
+  getFileMeta: (file, field) => {
+    const fileIndex = get().checkedFiles.findIndex(
+      (checked) => checked.url === file.url
+    );
+    return get().checkedFiles[fileIndex].metadata[field];
+  },
+
+  // isAllFilesChecked: () => null,
+  // addAllCheckedFiles: () => null,
+  removeAllCheckedFiles: () => set(() => ({ checkedFiles: [] })),
+
+  formData: {
+    files: [],
+    metadata: {
+      dataRelease: "",
+      readAndAdhere: false,
+      disclosureControls: false,
+      fileTypes: false,
+    },
+  },
+  setFormDataFiles: (files) => set(() => ({ formData: { files } })),
+  setFormDataMeta: (field, val) =>
+    set((state) => ({
+      formData: {
+        ...state.formData,
+        metadata: { ...state.formData.metadata, [field]: val },
+      },
+    })),
+  getFormDataMeta: (field) => get().formData?.metadata?.[field],
+}));
+
+export default useFileStore;
+
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("Store1", useFileStore);
+}
