f883686cf2ae0bab101660ef7c79c6f297367ede
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-08-30 15:44:38 +0100

    Revert releases UI changes

--- assets/src/scripts/outputs-viewer/components/Metadata/Metadata.jsx
+++ assets/src/scripts/outputs-viewer/components/Metadata/Metadata.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import prettyFileSize from "../../utils/pretty-file-size";
 import { selectedFileProps } from "../../utils/props";
 
-function Metadata({ fileDate, fileName, fileSize, fileUrl, metadata }) {
+function Metadata({ fileDate, fileName, fileSize, fileUrl }) {
   const fileDateValue = () => {
     if (Date.parse(fileDate)) {
       const getDate = new Date(fileDate);
@@ -18,14 +18,6 @@ function Metadata({ fileDate, fileName, fileSize, fileUrl, metadata }) {
     }
     return false;
   };
-
-  const renderMetadata = (data) =>
-    Object.keys(data).map((key) => (
-      <div key={key} className="mt-3">
-        <strong>{key}</strong>
-        <div>{data[key]}</div>
-      </div>
-    ));
 
   return (
     <div className="card-header">
@@ -54,7 +46,6 @@ function Metadata({ fileDate, fileName, fileSize, fileUrl, metadata }) {
         )}
         <li className="list-inline-item spacer">{prettyFileSize(fileSize)}</li>
       </ul>
-      <div>{metadata && renderMetadata(metadata)}</div>
     </div>
   );
 }
@@ -66,9 +57,4 @@ Metadata.propTypes = {
   fileName: selectedFileProps.name.isRequired,
   fileSize: selectedFileProps.size.isRequired,
   fileUrl: selectedFileProps.url.isRequired,
-  metadata: selectedFileProps.metadata,
-};
-
-Metadata.defaultProps = {
-  metadata: null,
 };
