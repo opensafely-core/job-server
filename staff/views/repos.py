f3be5f18c3caa213a0f1988b794c5b0142f24887
George Hickman <george@ghickman.co.uk>
2022-09-08 16:20:38 +0100

    Add RepoDetail page to Staff Area

--- staff/views/repos.py
+++ staff/views/repos.py
@@ -1,21 +1,100 @@
 from datetime import timedelta
+from urllib.parse import quote, unquote
 
 import structlog
 from django.db.models import Count, Min
-from django.db.models.functions import Least
+from django.db.models.functions import Least, Lower
 from django.template.response import TemplateResponse
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from first import first
+from furl import furl
 
 from jobserver.authorization import CoreDeveloper
 from jobserver.authorization.decorators import require_role
 from jobserver.github import _get_github_api
-from jobserver.models import Workspace
+from jobserver.models import Job, User, Workspace
 
 
 logger = structlog.get_logger(__name__)
+
+
+def ran_at(job):
+    return job.started_at or job.created_at
+
+
+@method_decorator(require_role(CoreDeveloper), name="dispatch")
+class RepoDetail(View):
+    get_github_api = staticmethod(_get_github_api)
+
+    def get(self, request, *args, **kwargs):
+        url = unquote(self.kwargs["repo_url"])
+
+        org, name = furl(url).path.segments
+        repo = self.get_github_api().get_repo(org, name)
+
+        workspaces = (
+            Workspace.objects.filter(repo=url)
+            .select_related("created_by", "project", "project__org")
+            .order_by("name")
+        )
+        users = User.objects.filter(job_requests__workspace__in=workspaces).distinct()
+
+        jobs = Job.objects.filter(job_request__workspace__in=workspaces).annotate(
+            first_run=Min(Least("started_at", "created_at"))
+        )
+        first_job_ran_at = ran_at(jobs.order_by("first_run").first())
+        last_job_ran_at = ran_at(jobs.order_by("-first_run").first())
+
+        twelve_month_limit = first_job_ran_at + timedelta(days=365)
+
+        def build_workspace(workspace, all_users):
+            """
+            Build a dictionary representation of the Workspace data for the template
+
+            We want to show a list of users-who-created-jobs-in-this-workspace
+            in the template.  Getting this as part of the Workspace QuerySet
+            doesn't appear to be possible (we tried Subquery and Prefetch to no
+            avail).  Instead we've looked up the users who created jobs for
+            each workspace used in this view and are pairing them up here.
+            """
+
+            # get the users who created jobs in this workspace or created it,
+            # just in case the creator has not run any jobs.
+            users = list(
+                all_users.filter(job_requests__workspace=workspace)
+                .distinct()
+                .order_by(Lower("username"), Lower("fullname"))
+            )
+            if workspace.created_by not in users:
+                users = [workspace.created_by, *users]
+
+            return {
+                "created_by": workspace.created_by,
+                "get_staff_url": workspace.get_staff_url,
+                "is_archived": workspace.is_archived,
+                "job_requesting_users": users,
+                "name": workspace.name,
+            }
+
+        workspaces = [build_workspace(w, users) for w in workspaces]
+
+        context = {
+            "first_job_ran_at": first_job_ran_at,
+            "has_releases": "github-releases" in repo["topics"],
+            "last_job_ran_at": last_job_ran_at,
+            "repo": repo,
+            "repo_url": url,
+            "twelve_month_limit": twelve_month_limit,
+            "workspaces": workspaces,
+        }
+
+        return TemplateResponse(
+            request,
+            "staff/repo_detail.html",
+            context=context,
+        )
 
 
 @method_decorator(require_role(CoreDeveloper), name="dispatch")
@@ -84,6 +163,7 @@ class RepoList(View):
                 "first_run": first_run,
                 "has_jobs": has_jobs,
                 "has_releases": "github-releases" in repo["topics"],
+                "quoted_url": quote(repo["url"], safe=""),
                 "workspace": workspace,
                 "workspaces": workspaces,
             }
