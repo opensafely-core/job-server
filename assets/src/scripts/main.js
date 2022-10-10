976020622bfa26255b431f16ee19c5bc596ddaf4
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-10-10 13:45:03 +0100

    Update the Event Log to use a table element

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,3 +1,4 @@
+/* global $ */
 import "../styles/main.scss";
 
 if (document.location.hostname === "jobs.opensafely.org") {
@@ -8,4 +9,28 @@ if (document.location.hostname === "jobs.opensafely.org") {
   script.src = "https://plausible.io/js/plausible.compat.js";
 
   document.head.appendChild(script);
+}
+
+if (document.getElementById("eventLog")) {
+  document.querySelectorAll('[data-toggle="show-hide-btn"]').forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const childRow = document.getElementById(
+        btn.getAttribute("aria-controls")
+      );
+
+      if (childRow.hidden) {
+        childRow.hidden = false;
+        childRow.setAttribute("aria-hidden", false);
+        return btn.setAttribute("aria-expanded", true);
+      }
+
+      childRow.hidden = true;
+      childRow.setAttribute("aria-hidden", true);
+      return btn.setAttribute("aria-expanded", false);
+    });
+  });
+
+  $(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 }
