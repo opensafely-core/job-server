47d7406f53182049478e9edbf1db3b70ec198c37
George Hickman <george@ghickman.co.uk>
2022-11-03 08:58:05 +0000

    Look up file counts for copilot dashboard in a second query
    When annotating the number of files for a project the query projects
    query jumps from ~120ms to ~20s locally.  I can't see anything obviously
    wrong from the query planner and it seems to specifically be the files
    annotation causing this slow down.  So instead this does a separate
    query, grouping in memory by project and coverting to a dict of
    project_pk: number_of_files, which is merged with the project data.

--- tests/unit/staff/views/dashboards/test_copiloting.py
+++ tests/unit/staff/views/dashboards/test_copiloting.py
@@ -1,18 +1,50 @@
+from datetime import datetime, timezone
+
 import pytest
 from django.core.exceptions import PermissionDenied
 
 from staff.views.dashboards.copiloting import Copiloting
 
-from .....factories import UserFactory
+from .....factories import (
+    JobFactory,
+    JobRequestFactory,
+    ProjectFactory,
+    ReleaseFileFactory,
+    UserFactory,
+    WorkspaceFactory,
+)
 
 
 def test_copiloting_success(rf, core_developer):
+    project = ProjectFactory()
+    workspace = WorkspaceFactory(project=project)
+    ReleaseFileFactory.create_batch(15, workspace=workspace)
+
+    job_request1 = JobRequestFactory(workspace=workspace)
+    JobFactory(
+        job_request=job_request1, started_at=datetime(2020, 7, 31, tzinfo=timezone.utc)
+    )
+    job_request2 = JobRequestFactory(workspace=workspace)
+    JobFactory(
+        job_request=job_request2, started_at=datetime(2021, 9, 3, tzinfo=timezone.utc)
+    )
+
     request = rf.get("/")
     request.user = core_developer
 
     response = Copiloting.as_view()(request)
 
     assert response.status_code == 200
+
+    assert len(response.context_data["projects"])
+
+    project = response.context_data["projects"][0]
+    assert project["date_first_run"] == datetime(
+        2020, 7, 31, 0, 0, 0, tzinfo=timezone.utc
+    )
+    assert project["files_released_count"] == 15
+    assert project["job_request_count"] == 2
+    assert project["workspace_count"] == 1
 
 
 def test_copiloting_unauthorized(rf):
