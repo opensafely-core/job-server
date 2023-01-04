88d21023168390bc827ff061d6eca737842dbdf9
George Hickman <george@ghickman.co.uk>
2023-01-04 16:43:57 +0000

    Limit the study definition to 1500 characters in the form and UI

--- assets/src/scripts/application-form.js
+++ assets/src/scripts/application-form.js
@@ -8,3 +8,46 @@ const links = content.getElementsByTagName("a");
     link.rel = "noopener noreferrer";
   }
 });
+
+// Select the textarea element.
+const textAreaElement = document.getElementById("id_study_purpose");
+
+// Select the character counter element.
+const characterCounterElement = document.getElementById("character_counter");
+
+// Select the element that shows the number of characters typed in the textarea.
+const typedCharactersElement = document.getElementById("typed_characters");
+
+// Define the maximum number of characters allowed.
+const maximumCharacters = textAreaElement.getAttribute("max_length");
+
+// Add a "keydown" event listener on the textarea element.
+textAreaElement.addEventListener("keyup", () => {
+  // Count the number of characters typed.
+  const typedCharacters = textAreaElement.value.length;
+
+  /**
+   * Check if the typed characters is more than allowed characters limit.
+   * If yes, then return false.
+   */
+  if (typedCharacters > maximumCharacters) {
+    return false;
+  }
+  //
+  // Display the number of characters typed.
+  typedCharactersElement.textContent = typedCharacters;
+
+  /**
+   * Change the character counter text colour to "orange" if the typed
+   * characters number is between 200 to 250. If more, then change the colour to "red".
+   */
+  if (typedCharacters < 1400) {
+    characterCounterElement.classList = "";
+  } else if (typedCharacters >= 1400 && typedCharacters < 1450) {
+    characterCounterElement.classList = "text-warning";
+  } else if (typedCharacters >= 1450) {
+    characterCounterElement.classList = "text-danger";
+  }
+
+  return undefined;
+});
