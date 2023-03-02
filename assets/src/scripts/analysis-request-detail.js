292406e7ab9b2e1f7c67d6e65bab0fb350e196b6
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-03-02 14:12:11 +0000

    Style staff and public templates

--- assets/src/scripts/analysis-request-detail.js
+++ assets/src/scripts/analysis-request-detail.js
@@ -1,13 +1,1 @@
-import hljs from "highlight.js/lib/core";
-import json from "highlight.js/lib/languages/json";
-import "highlight.js/styles/github.css";
-
-hljs.registerLanguage("json", json);
-
-document.getElementById("analysisRequest").textContent = JSON.stringify(
-  JSON.parse(document.getElementById("analysisRequestData").textContent),
-  null,
-  2
-);
-
-hljs.highlightAll();
+// Silence
--- assets/src/scripts/staff.js
+++ assets/src/scripts/staff.js
@@ -1,5 +1,19 @@
 /* global $ */
 // Bootstrap tooltips
+import hljs from "highlight.js/lib/core";
+import json from "highlight.js/lib/languages/json";
+import "highlight.js/styles/github.css";
+
 $(() => {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+hljs.registerLanguage("json", json);
+
+document.getElementById("analysisRequest").textContent = JSON.stringify(
+  JSON.parse(document.getElementById("analysisRequestData").textContent),
+  null,
+  2
+);
+
+hljs.highlightAll();
