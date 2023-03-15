a2db8b9d4a43a74f3c9eee5533af6bda79dd6e6b
George Hickman <george@ghickman.co.uk>
2023-03-15 16:10:54 +0000

    Simplify get_redirect_url with furl
    Rather than working with QueryDict directly, which is both immutable and
    doesn't allow us to provide a default for .pop(), we can use furl to
    handle both problems.

--- tests/unit/staff/test_querystring_tools.py
+++
@@ -1,8 +0,0 @@
-from staff.querystring_tools import merge_query_args
-
-
-def test_merge_query_args(rf):
-    request = rf.get("/?one-arg=value&two-arg=value")
-
-    f = merge_query_args(request.GET, {"three-arg": "value"})
-    assert f.args == {"one-arg": "value", "two-arg": "value", "three-arg": "value"}
