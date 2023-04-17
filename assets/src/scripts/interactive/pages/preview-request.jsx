d3c1b042b00a7cd75d8d3eb399dc3af452c87999
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-04-17 10:27:47 +0100

    Fix preview request styling

--- assets/src/scripts/interactive/pages/preview-request.jsx
+++ assets/src/scripts/interactive/pages/preview-request.jsx
@@ -18,27 +18,26 @@ function PreviewRequest() {
       <AlertPage />
       <h1 className="text-4xl font-bold mb-8">Preview your request</h1>
 
-      <div className="max-w-lg relative py-12">
-        <p className="absolute max-w-[28ch] text-center top-0 right-0 border-2 border-oxford-300 bg-slate-50 p-2">
-          Individual has qualifying event from:
-          <br />
-          <strong>{formData.codelistA.label}</strong>
-        </p>
-
-        <div className="pt-12 pb-6">
+      <div className="px-4 py-24">
+        <div className="relative">
+          <p className="absolute max-w-[28ch] text-center -translate-y-[80%] top-0 right-0 border-2 border-oxford-300 bg-slate-50 p-2">
+            Individual has qualifying event from:
+            <br />
+            <strong>{formData.codelistA.label}</strong>
+          </p>
           <EventsBefore
             timeScale={formData.timeScale}
             timeValue={formData.timeValue}
           />
+          <p className="absolute max-w-[28ch] text-center bottom-0 translate-y-[80%] left-0 border-2 border-oxford-300 bg-slate-50 p-2">
+            Individual ALSO has an event from:
+            <br />
+            <strong>{formData.codelistB.label}</strong>
+          </p>
         </div>
-        <p className="absolute max-w-[28ch] text-center bottom-0 left-0 border-2 border-oxford-300 bg-slate-50 p-2">
-          Individual ALSO has an event from:
-          <br />
-          <strong>{formData.codelistB.label}</strong>
-        </p>
       </div>
 
-      <div className="flex flex-row w-full gap-2 mt-10">
+      <div className="flex flex-row w-full gap-2 mt-16">
         <Button onClick={() => navigate("filter-request")}>Next</Button>
       </div>
     </>
