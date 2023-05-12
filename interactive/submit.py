d7c68bccfdfdce7ed58728f6cd5d9788671327a1
bloodearnest <simon.davy@thedatalab.org>
2023-05-12 12:14:31 +0100

    Add code to resubmit an analysis request.
    It will run the code that is tagged with the analysis id in the repo, so
    if we update it via some other method (e.g. to fix a bug in
    interactive-templates) it should work then too.

--- interactive/submit.py
+++ interactive/submit.py
@@ -1,7 +1,10 @@
 from attrs import asdict
 from django.conf import settings
 from django.db import transaction
+from furl import furl
 from interactive_templates import create_commit
+
+from jobserver.github import _get_github_api
 
 from .models import AnalysisRequest
 from .slacks import notify_analysis_request_submitted
@@ -50,18 +53,70 @@ def submit_analysis(
         force=force,
     )
 
-    job_request = project.interactive_workspace.job_requests.create(
+    create_job_request(analysis_request, backend, sha, project_yaml)
+
+    analysis_request.save()
+    notify_analysis_request_submitted(analysis_request)
+
+    return analysis_request
+
+
+def create_job_request(analysis_request, backend, sha, project_yaml):
+    workspace = analysis_request.project.interactive_workspace
+    job_request = workspace.job_requests.create(
         backend=backend,
-        created_by=creator,
+        created_by=analysis_request.created_by,
         sha=sha,
         project_definition=project_yaml,
         force_run_dependencies=True,
         requested_actions=["run_all"],
     )
-
     analysis_request.job_request = job_request
     analysis_request.save()
 
-    notify_analysis_request_submitted(analysis_request)
 
-    return analysis_request
+def resubmit_analysis(analysis_request, github_api=None):
+    if github_api is None:  # noqa
+        github_api = _get_github_api()
+    repo_url = analysis_request.project.interactive_workspace.repo.url
+    sha, project_yaml = get_existing_commit(analysis_request.id, repo_url, github_api)
+    create_job_request(
+        analysis_request, analysis_request.job_request.backend, sha, project_yaml
+    )
+
+
+def get_existing_commit(analysis_id, repo_url, github_api):
+    if repo_url.startswith("http"):
+        return _get_existing_commit_github(analysis_id, repo_url, github_api)
+    else:  # support local repositories for testing
+        return _get_existing_commit_local(analysis_id, repo_url)
+
+
+def _get_existing_commit_github(analysis_id, repo_url, github_api):
+    """Used for github repos, in production."""
+    url = furl(repo_url)
+    org, repo = url.path.segments[:2]
+    sha = github_api.get_tag_sha(org, repo, analysis_id)
+    project_yaml = github_api.get_file(org, repo, analysis_id)
+    return sha, project_yaml
+
+
+def _get_existing_commit_local(analysis_id, repo_url):
+    """Used for local repo, in testing."""
+    from interactive_templates.create import git
+
+    ps = git(
+        "ls-remote",
+        "--tags",
+        repo_url,
+        f"refs/tags/{analysis_id}",
+        capture_output=True,
+        check=True,
+    )
+
+    sha = ps.stdout[:40]
+
+    ps = git("show", f"{sha}:project.yaml", capture_output=True, cwd=repo_url)
+    project_yaml = ps.stdout.strip()
+
+    return sha, project_yaml
