a9ab72ae43cb49fd00596e790e72449099cbaccf
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-07-21 16:08:41 +0100

    Replace Alpine dropdown with custom vanilla JS

--- assets/src/scripts/tw.js
+++ assets/src/scripts/tw.js
@@ -1,32 +1,6 @@
-/* eslint-disable no-return-assign */
-import Screen from "@alpine-collective/toolkit-screen/dist/module.esm";
 import "@zachleat/details-utils";
-import Alpine from "alpinejs/packages/csp/dist/module.esm";
 import "../styles/tw.css";
 import "./_alert";
+import "./_expand-list";
 import "./_plausible";
 import "./utils/double-click";
-
-Alpine.plugin(Screen);
-
-document.addEventListener("alpine:init", () => {
-  Alpine.data("dropdown", () => ({
-    open: false,
-
-    init() {
-      this.$refs.dialogue.classList.add("hidden");
-    },
-
-    trigger: {
-      // eslint-disable-next-line func-names
-      "@click": function () {
-        this.open = !this.open;
-        this.$refs.dialogue.classList.toggle("hidden");
-        this.$refs.dialogue.setAttribute("aria-expanded", this.open);
-      },
-    },
-  }));
-});
-
-window.Alpine = Alpine;
-window.Alpine.start();
