a5ed6813c635def2fc86183459bfb4ddcf4b516d
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-07-21 15:14:48 +0100

    Replace Alpine.js in header with details-utils
    Use separate nav bars for desktop and mobile views

--- assets/src/scripts/tw.js
+++ assets/src/scripts/tw.js
@@ -1,60 +1,15 @@
 /* eslint-disable no-return-assign */
 import Screen from "@alpine-collective/toolkit-screen/dist/module.esm";
+import "@zachleat/details-utils";
 import Alpine from "alpinejs/packages/csp/dist/module.esm";
 import "../styles/tw.css";
-import "./utils/double-click";
 import "./_alert";
 import "./_plausible";
+import "./utils/double-click";
 
 Alpine.plugin(Screen);
 
 document.addEventListener("alpine:init", () => {
-  Alpine.data("header", () => ({
-    get largerThanMd() {
-      return this.$screen("md");
-    },
-
-    showMainNav: false,
-    get isMainNavOpen() {
-      if (this.largerThanMd) {
-        return true;
-      }
-      return this.showMainNav;
-    },
-    clickMainNav() {
-      if (this.largerThanMd) {
-        this.showMainNav = true;
-      }
-      return (this.showMainNav = !this.showMainNav);
-    },
-    closeMainNav() {
-      if (this.largerThanMd) {
-        return (this.showMainNav = true);
-      }
-      return (this.showMainNav = false);
-    },
-
-    showUserNav: false,
-    get isUserNavOpen() {
-      if (!this.largerThanMd) {
-        return true;
-      }
-      return this.showUserNav;
-    },
-    clickUserNav() {
-      if (this.largerThanMd) {
-        return (this.showUserNav = !this.showUserNav);
-      }
-      return (this.showUserNav = true);
-    },
-    closeUserNav() {
-      if (this.largerThanMd) {
-        return (this.showUserNav = false);
-      }
-      return null;
-    },
-  }));
-
   Alpine.data("dropdown", () => ({
     open: false,
 
