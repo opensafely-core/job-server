f98752e63feab9369bff8b03ce81566f0229104f
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-08-01 13:46:52 +0100

    Remove Bootstrap styles from Outputs SPA

--- assets/src/scripts/outputs-viewer/index.jsx
+++ assets/src/scripts/outputs-viewer/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "../../styles/outputs-viewer.scss";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
