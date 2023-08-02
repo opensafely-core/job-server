612e7d14a2aedf287dc67fc8a23eb9c676f79a92
George Hickman <george@ghickman.co.uk>
2023-08-02 11:58:50 +0100

    Switch the publish_files command to use paths and a workspace
    This allows users to provide us with a list of paths without having to
    convert to PKs.

--- jobserver/management/commands/publish_files.py
+++ jobserver/management/commands/publish_files.py
@@ -2,33 +2,44 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from furl import furl
 
-from jobserver import models
+from jobserver import models, releases
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("username", type=str)
         parser.add_argument(
-            "ids", nargs="+", type=str, help="ReleaseFile IDs to add to Snapshot"
+            "username", type=str, help="Username to create the Snapshot with"
+        )
+        parser.add_argument(
+            "workspace", type=str, help="Workspace name to look up files under"
+        )
+        parser.add_argument(
+            "paths", nargs="+", type=str, help="File paths to add to Snapshot"
         )
 
     def handle(self, *args, **options):
-        ids = set(options["ids"])
+        paths = set(options["paths"])
         username = options["username"]
+        workspace = options["workspace"]
 
         try:
             user = models.User.objects.get(username=username)
         except models.User.DoesNotExist:
             raise CommandError(f"Unknown username: {username}")
 
-        rfiles = models.ReleaseFile.objects.filter(pk__in=ids)
+        try:
+            workspace = models.Workspace.objects.get(name=workspace)
+        except models.Workspace.DoesNotExist:
+            raise CommandError(f"Unknown workspace: {workspace}")
 
-        found = set(rfiles.values_list("pk", flat=True))
-        if unknown := ids - found:
-            raise CommandError(f"Unknown ReleaseFiles: {', '.join(unknown)}")
+        latest = set(releases.workspace_files(workspace).values())
+        requested = [f for f in latest if f.name in paths]
+
+        if missing := paths - {f.name for f in requested}:
+            raise CommandError(f"Unknown paths: {', '.join(missing)}")
 
         publish_request = models.PublishRequest.create_from_files(
-            files=rfiles, user=user
+            files=requested, user=user
         )
 
         url = furl(settings.BASE_URL) / publish_request.snapshot.get_absolute_url()
