397688ed3cf076affe652dfc12dc2407b2991394
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-08-02 13:29:39 +0100

    Replace postcss-url with Vite server config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,9 @@
-import path from "node:path";
-
-export default ({ env }) => ({
+export default {
   plugins: {
     "postcss-import": {},
     "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
     autoprefixer: {},
     "postcss-preset-env": {},
-    "postcss-url":
-      env !== "production"
-        ? {
-            url: "inline",
-            basePath: path.resolve("assets/src/styles"),
-          }
-        : false,
   },
-});
+};
