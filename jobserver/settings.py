opensafely-core/job-server

commit 6832a34e41b3ac5db021bd25a56d398bbc67f3b6

Switch to Paths throughout settings
We're using BASE_DIR as a Path elsewhere, this change makes it
consistent throughout settings too.

jobserver/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
@@ -23,7 +22,7 @@ from services.sentry import initialise_sentry
 env = Env()
 env.read_env()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: BASE_DIR / ...
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -155,16 +154,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    env.str("BUILT_ASSETS", default=os.path.join(BASE_DIR, "assets", "dist")),
+    BASE_DIR / "static",
+    env.path("BUILT_ASSETS", default=BASE_DIR / "assets" / "dist"),
 ]
-STATIC_ROOT = env.str("STATIC_ROOT", default=os.path.join(BASE_DIR, "staticfiles"))
+STATIC_ROOT = env.path("STATIC_ROOT", default=BASE_DIR / "staticfiles")
 STATIC_URL = "/static/"
 
 DJANGO_VITE_DEV_SERVER_PORT = 5173
 DJANGO_VITE_ASSETS_PATH = "/static/"
 DJANGO_VITE_DEV_MODE = env.bool("DJANGO_VITE_DEV_MODE", default=False)
-DJANGO_VITE_MANIFEST_PATH = os.path.join(STATIC_ROOT, "manifest.json")
+DJANGO_VITE_MANIFEST_PATH = STATIC_ROOT / "manifest.json"
 
 # Insert Whitenoise Middleware.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"

commit 336a6d0d5bcbab03bfafd5639218cfc8cfe440a4

Fix the collect-me-maybe script regression.
This had been previously fixed, but was broken again in
6832a34e41b3ac5db021bd25a56d398bbc67f3b6, for 2+ months.

The collect-me-maybe.sh script relies on strings being output by
`manage.py print_settings`. If they are path objects, it outputs the
repr for them, which is not valid path.

To prevent this regressing again, I have added comment and a validation
check to the script.

jobserver/settings.py
@@ -163,9 +163,10 @@ STORAGES = {
 }
 
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# Note: these *must* be strings. If they are paths, we cannot cleanly extract them in ./scripts/collect-me-maybe.sh
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    env.path("BUILT_ASSETS", default=BASE_DIR / "assets" / "dist"),
+    str(BASE_DIR / "static"),
+    str(env.path("BUILT_ASSETS", default=BASE_DIR / "assets" / "dist")),
 ]
 STATIC_ROOT = env.path("STATIC_ROOT", default=BASE_DIR / "staticfiles")
 STATIC_URL = "/static/"

scripts/collect-me-maybe.sh
@@ -18,6 +18,11 @@ if ! test -f "$sentinel"; then
     run=true
 else
     staticdirs="$($python manage.py print_settings STATICFILES_DIRS --format value | sed -e "s/[]',[]//g")"
+
+    if [[ "$staticdirs" == *"PosixPath"* ]]; then
+        echo "ERROR: invalid STATICFILES_DIRS values - they should be strings not paths"
+        exit 1
+    fi
     # shellcheck disable=SC2086
     find $staticdirs -type f -newer "$sentinel" | grep -q . && run=true
 fi