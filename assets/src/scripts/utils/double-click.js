3eac1eaa13682d57edbb82dbd76298041f7cfc0e
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-10-25 11:33:11 +0100

    Update double-click script to improve UX

--- assets/src/scripts/utils/double-click.js
+++ assets/src/scripts/utils/double-click.js
@@ -8,12 +8,27 @@ document
   )
   .forEach((btn) => {
     btn.addEventListener("click", async (e) => {
-      // Wait for 1ms to trigger all button onClick events
-      await wait(1);
+      if (e.target instanceof HTMLElement) {
+        // Wait for 1ms to trigger all button onClick events
+        await wait(1);
 
-      // Disable the button, wait 1s then reenable
-      e.target.setAttribute("disabled", true);
-      await wait(1000);
-      e.target.removeAttribute("disabled");
+        // Disable the button
+        e.target.setAttribute("disabled", "true");
+        e.target.classList.add(
+          "!cursor-not-allowed",
+          "!bg-slate-300",
+          "!border-slate-300",
+          "!text-slate-800",
+        );
+
+        await wait(30000);
+        e.target.removeAttribute("disabled");
+        e.target.classList.remove(
+          "!cursor-not-allowed",
+          "!bg-slate-300",
+          "!border-slate-300",
+          "!text-slate-800",
+        );
+      }
     });
   });
