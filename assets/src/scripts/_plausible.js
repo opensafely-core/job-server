62ef158f79e899e530349b79b87f50b9f517a3ff
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-01-09 16:31:58 +0000

    Add Plausible events for logged in + staff status

--- assets/src/scripts/_plausible.js
+++ assets/src/scripts/_plausible.js
@@ -1,9 +1,20 @@
+const isLoggedIn = document.head.querySelector(`meta[name="is_logged_in"]`);
+const isStaff = document.head.querySelector(`meta[name="is_staff"]`);
+
 if (document.location.hostname === "jobs.opensafely.org") {
   const script = document.createElement("script");
   script.defer = true;
   script.setAttribute("data-domain", "jobs.opensafely.org");
   script.id = "plausible";
-  script.src = "https://plausible.io/js/plausible.compat.js";
+  script.src = "https://plausible.io/js/script.pageview-props.js";
+
+  if (isLoggedIn) {
+    script.setAttribute("event-is_logged_in", isLoggedIn.content);
+  }
+
+  if (isStaff) {
+    script.setAttribute("event-is_staff", isStaff.content);
+  }
 
   document.head.appendChild(script);
 }
