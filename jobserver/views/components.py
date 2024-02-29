2b63ca3c615fe593315cd8d6a596633968045c8b
Becky Smith <becky.smith@thedatalab.org>
2024-02-29 14:38:38 +0000

    Use a Django form for example form components
    This ensures that where we show an example that uses a Django form
    field, it does work properly with an actual field. (E.g. previously
    the example select used choices=<fieldname>.choices, whereas
    actual use with a formfield is <fieldname>.field.choices

--- jobserver/views/components.py
+++ jobserver/views/components.py
@@ -1,51 +1,42 @@
 from datetime import UTC, datetime
 
+from django import forms
 from django.template.response import TemplateResponse
 
 
-def components(request):
-    example_date = datetime.fromtimestamp(1667317153, tz=UTC)
-    example_form_email = {
-        "auto_id": "id_email",
-        "errors": {"This email is registered to a different account"},
-        "html_name": "email",
-        "id_for_label": "id_email",
-        "label": "Email address",
-        "value": "you@example.com",
-    }
+class ExampleForm(forms.Form):
 
-    example_form_select = {
-        "auto_id": "id_language_select",
-        "choices": [
+    example_select = forms.ChoiceField(
+        choices=[
             ["", "Please select a language"],
             ["english", "English"],
             ["french", "French"],
             ["german", "German"],
             ["spanish", "Spanish"],
         ],
-        "errors": {},
-        "html_name": "language-select",
-        "id_for_label": "id_language_select",
-        "label": "Language select",
-        "value": "french",
-    }
+        label="Language select",
+        initial="french",
+    )
 
-    example_form_textarea = {
-        "auto_id": "id_project_description",
-        "errors": {},
-        "html_name": "project-description",
-        "id_for_label": "id_project_description",
-        "label": "Notification email address",
-        "value": "",
-    }
+    example_email = forms.EmailField()
 
+    example_textarea = forms.Textarea()
+
+    def clean_example_email(self):
+        self.add_error(
+            "example_email", "This email is registered to a different account"
+        )
+
+
+def components(request):
+    example_date = datetime.fromtimestamp(1667317153, tz=UTC)
+    form = ExampleForm({"example_select": "french", "example_email": "you@example.com"})
+    form.is_valid()
     return TemplateResponse(
         request,
         "_components/index.html",
         context={
             "example_date": example_date,
-            "example_form_email": example_form_email,
-            "example_form_select": example_form_select,
-            "example_form_textarea": example_form_textarea,
+            "example_form": form,
         },
     )
