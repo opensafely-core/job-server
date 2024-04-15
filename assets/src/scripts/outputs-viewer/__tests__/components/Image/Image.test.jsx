aecec096760c5bfec6b454c9008b1d0833853ef7
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-04-15 10:06:07 +0100

    Fix image tag tests

--- assets/src/scripts/outputs-viewer/__tests__/components/Image/Image.test.jsx
+++ assets/src/scripts/outputs-viewer/__tests__/components/Image/Image.test.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { describe, expect, it } from "vitest";
 import Image from "../../../components/Image/Image";
 import { pngExample } from "../../helpers/files";
-import { render, screen } from "../../test-utils";
+import { render } from "../../test-utils";
 
 describe("<Image />", () => {
   it("shows an image if a data blob is passed", async () => {
-    render(<Image data={pngExample} />);
-
-    expect(screen.getByRole("img").src).toBe(pngExample);
+    const { container } = render(<Image data={pngExample} />);
+    expect(container.querySelector("img").getAttribute("src")).toBe(pngExample);
   });
 });
