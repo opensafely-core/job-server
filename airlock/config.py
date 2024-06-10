466586bb0b2da181e0ffee661ae25af998bef2bd
Becky Smith <becky.smith@thedatalab.org>
2024-06-10 13:17:43 +0100

    Allow configuration of org/repo for airlock issues by organisation
    This allows  output-checking issues to be opened in custom repos,
    depending on organisation. Airlock can also send configuration
    to use, which allows an entire backend to use a different repo (e.g.
    the test backend, which we don't want to use a real output checking
    repo)

---
+++ airlock/config.py
@@ -0,0 +1,7 @@
+ORG_OUTPUT_CHECKING_REPOS = {
+    "university-of-bristol": {
+        "org": "ebmdatalab",
+        "repo": "opensafely-output-review-bristol",
+    },
+    "default": {"org": "ebmdatalab", "repo": "opensafely-output-review"},
+}
