53a1a1b01fb7e308799526ec32ae309845b43e83
Iain Dillingham <iain@dillingham.me.uk>
2024-07-31 14:59:47 +0100

    Add `MissingVariableErrorFilter`
    Instances of `LogRecord` are now created for "missing template variable"
    log messages. These instances are passed to
    `MissingVariableErrorFilter`, converted to warnings (although there are
    exceptions), and rejected.

--- tests/unit/services/test_logging.py
+++ tests/unit/services/test_logging.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+import pytest
+from django.template import Context, Template
+
 from services.logging import timestamper
 
 
@@ -17,3 +20,25 @@ def test_timestamper_without_debug(monkeypatch):
     monkeypatch.setattr("services.logging.DEBUG", False)
 
     assert timestamper(None, None, {"event": "derp"}) == {"event": "derp"}
+
+
+def test_missing_variable_error_filter():
+    template = Template("*{{ my_missing_variable }}*", name="index.html")
+    context = Context({"my_variable": "my_value"})
+
+    with pytest.warns(UserWarning):
+        template.render(context)
+
+
+def test_missing_variable_error_filter_with_ignored_prefix():
+    template = Template("*{{ my_missing_variable }}*", name="admin/index.html")
+    context = Context({"my_variable": "my_value"})
+
+    template.render(context) == "**"
+
+
+def test_missing_variable_error_filter_with_ignored_variable_name():
+    template = Template("*{{ csp_nonce }}*", name="index.html")
+    context = Context({"csp_nonce": "csp_nonce_value"})
+
+    template.render(context) == "*csp_nonce_value*"
