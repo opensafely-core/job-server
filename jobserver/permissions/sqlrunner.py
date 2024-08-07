cca345bf938d68e095c97018ea63a70268f87066
David Evans <dave.evans@thedatalab.org>
2024-08-07 15:25:36 +0100

    Add reminders to update documentation if needed
    It's not obvious to someone editing these files that they are referenced
    elsewhere so we add some breadcrumbs/back-links here.
    
    For discussion on the limitations of CODEOWNERS see:
    https://bennettoxford.slack.com/archives/C058N7P4T2S/p1721723419112229?thread_ts=1721312706.728699&cid=C058N7P4T2S

--- jobserver/permissions/sqlrunner.py
+++ jobserver/permissions/sqlrunner.py
@@ -1,6 +1,14 @@
-# ***************************************************************************
-# ANY CHANGES TO THE LOGIC BELOW MUST BE EXPLICITLY APPROVED BY THE IG TEAM *
-# ***************************************************************************
+# *******************
+#   IMPORTANT NOTE  *
+# *******************
+#
+# Only the IG team are authorised to make modifications to the list of projects below.
+# Any PRs which update this list should be reviewed and approved by a member of the IG
+# team so as to create the appropriate audit trail.
+#
+# Note also that this file is linked to in the documentation. If you move or restructure
+# this file you should ensure the documentation is updated appropriately.
+# https://github.com/opensafely/documentation/blob/af88baf1/docs/type-one-opt-outs.md
 
 
 def project_is_permitted_to_use_sqlrunner(project):
