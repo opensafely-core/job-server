df32f2e57fb33a93f8ebd62d2c7e52909f19fdfb
Steven Maude <git@stevenmaude.co.uk>
2024-08-15 19:17:21 +0100

    Remove `first` entirely

--- jobserver/first.py
+++
@@ -1,5 +0,0 @@
-def first(iterable, default=None, key=None):
-    if key is None:
-        return next((i for i in iterable if i), default)
-    else:
-        return next((i for i in iterable if key(i)), default)
