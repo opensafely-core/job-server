9f9b76d3488edc9192b355ac7b2a90887e78905a
Mike Kelly <mike.kelly@thedatalab.org>
2024-11-06 08:50:02 +0000

    Mark up tests with `@pytest.mark.disable_db`.
    This significantly speeds up executing the tests in isolation as the test DB
    doesn't need to be spun up and prepared.

--- tests/verification/test_github.py
+++ tests/verification/test_github.py
@@ -21,6 +21,7 @@ from .utils import assert_deep_type_equality, assert_public_method_signature_equ
 pytestmark = [pytest.mark.verification, pytest.mark.disable_db]
 
 
+@pytest.mark.disable_db
 class TestGitHubAPIAgainstFakes:
     def test_fake_public_method_signatures(self):
         """Test that `FakeGitHubAPI` has the same public methods with the same
@@ -65,6 +66,7 @@ def github_api():
     return GitHubAPI(token=Env().str("GITHUB_TOKEN_TESTING"))
 
 
+@pytest.mark.disable_db
 @pytest.mark.usefixtures("enable_network")
 class TestGithubAPIPrivate:
     """Tests of the real GitHubAPI that require permissions on a private
@@ -227,6 +229,7 @@ class TestGithubAPIPrivate:
         assert repo["topics"] == ["testing"]
 
 
+@pytest.mark.disable_db
 @pytest.mark.usefixtures("enable_network")
 class TestGithubAPINonPrivate:
     """Tests of the real GitHubAPI that don't require permissions on a private
