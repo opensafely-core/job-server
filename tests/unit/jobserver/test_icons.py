cac5259d31a790213010f2ad7ef9df01da93679b
Steven Maude <git@stevenmaude.co.uk>
2025-02-27 13:16:08 +0000

    Remove use of `pytest-subtests`
    Fixes #4834.
    
    Refactor the single collection of tests that used `pytest-subtests` and
    use `parametrize()` instead.
    
    If we used the package more, maybe we could justify leaving it be, but
    it seems a good candidate for dependency removal.
    
    It also unlocks removing `attrs` (#4860).
    
    This does seem to slow the test down very slightly (running this set of
    tests alone takes about 6 seconds compared to 5 before this commit,
    though most of this is the pytest/xdist startup time).

--- tests/unit/jobserver/test_icons.py
+++ tests/unit/jobserver/test_icons.py
@@ -2,29 +2,48 @@ import itertools
 import re
 from pathlib import Path
 
+import pytest
 
-def test_all_icons(subtests):
-    """Ensure icon tag names defined in templates are valid tags"""
+
+# Logically, this feels like it could be a pytest.fixture.
+# However, it's not currently possible to use fixtures with parametrize:
+# https://github.com/pytest-dev/pytest/issues/349
+def icons():
     templates = [p for p in Path("templates").glob("**/*.html")]
 
-    # remove templates with no icon tag so we can simplify the regex matching
-    # code below
+    # Remove templates with no icon tag so we can simplify the regex matching
+    # code below.
     icon_templates = [p for p in templates if "{% icon_" in p.read_text()]
 
-    # look for the start of an icon templatetag and capture the full name
+    # Look for the start of an icon templatetag and capture the full name.
     icon_pat = re.compile(r"{% (icon_[^\s]+)")
 
-    # get a set of icon names in the Django templates
-    icons = set(
-        itertools.chain.from_iterable(
-            icon_pat.search(t.read_text()).groups() for t in icon_templates
+    # Get a sorted list of non-duplicated icon names in the Django templates.
+    # We use a sorted list instead of only a set
+    # to make this test work with parametrize and xdist:
+    # https://pytest-xdist.readthedocs.io/en/stable/known-limitations.html#order-and-amount-of-test-must-be-consistent
+    icons = sorted(
+        set(
+            itertools.chain.from_iterable(
+                icon_pat.search(t.read_text()).groups() for t in icon_templates
+            )
         )
     )
 
-    # read the component config contents into memory so we don't have to open
-    # it for each icon
-    config = Path("templates/components.yaml").read_text()
-    for icon in icons:
-        # use subtests to avoid failing the test at the first broken icon
-        with subtests.test(icon=icon):
-            assert icon in config
+    return icons
+
+
+@pytest.fixture(scope="module")
+def component_config():
+    # This is scoped to ideally to read the component config contents once,
+    # so we don't have to read it for each icon.
+    # Note: this fixture does not get reused with pytest-xdist,
+    # but in our case does not have any ill effect apart from duplicating work.
+    # https://github.com/pytest-dev/pytest-xdist/issues/783
+    return Path("templates/components.yaml").read_text()
+
+
+@pytest.mark.parametrize("icon", icons())
+def test_all_icons(icon, component_config):
+    """Ensure icon tag names defined in templates are valid tags"""
+    assert icon in component_config
