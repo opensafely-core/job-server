ddd62303f82d38347c88ccbc295dbf6186b16d8d
Steven Maude <git@stevenmaude.co.uk>
2025-03-04 17:40:37 +0000

    Remove Interactive from Jobs API
    Fixes #4878.
    
    We're removing the OpenSAFELY Interactive functionality, and this is
    only used there.
    
    This removes a check for succeeded/failed states of Interactive analysis
    requests, and the respective GitHub issue creation/Slack notification.

--- interactive/slacks.py
+++ interactive/slacks.py
@@ -9,23 +9,6 @@ from jobserver.utils import strip_whitespace
 from services import slack
 
 
-def notify_tech_support_of_failed_analysis(job_request, channel="tech-support-channel"):
-    """
-    Notify tech support when an Interactive analysis fails
-
-    Interactive users aren't expected to track the status of their job requests
-    in job-server because we show them a higher level interface, so it's
-    reasonable to think they would not be monitoring for failed jobs.  A failed
-    job for an Interactive request also likely represents a bug since the code
-    is written by OpenSAFELY, and tech support will triage this for us.
-    """
-    job_request_url = slack.link(job_request.get_absolute_url(), "view it here")
-
-    message = f"An analysis request has failed, {job_request_url}."
-
-    slack.post(message, channel)
-
-
 def notify_report_uploaded(analysis_request, channel="interactive-requests"):
     analysis_url = slack.link(analysis_request.get_absolute_url(), "View on job-server")
 
