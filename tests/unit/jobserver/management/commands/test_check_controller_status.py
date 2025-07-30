9d9236e810645f12d0ea0e2df13c22097fddd0f9
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-07-30 16:53:35 +0100

    Test api authorisation

--- tests/unit/jobserver/management/commands/test_check_controller_status.py
+++ tests/unit/jobserver/management/commands/test_check_controller_status.py
@@ -1,4 +1,5 @@
 import responses
+import responses.matchers
 from django.conf import settings
 
 from jobserver.management.commands.check_controller_status import Command as cd
@@ -8,8 +9,15 @@ def test_check_controller_status():
     with responses.RequestsMock() as rsps:
         # default rap api url
         url = settings.RAP_API_ENDPOINT
+        api_token = settings.RAP_API_TOKEN
 
-        rsps.add(responses.GET, url, body=b'{"method": "GET"}', status=200)
+        rsps.add(
+            responses.GET,
+            f"{url}/backend/status",
+            body=b'{"method": "GET"}',
+            match=[responses.matchers.header_matcher({"Authorization": api_token})],
+            status=200,
+        )
 
-        result = cd.check_controller_status(url)
+        result = cd.check_controller_status(url, api_token)
         assert result == b'{"method": "GET"}'
