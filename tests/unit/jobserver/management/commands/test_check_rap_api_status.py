e691be39d16d65840b923015156a3e543ee2ea22
Tom Ward <tomward@fmail.co.uk>
2025-07-31 13:10:18 +0100

    Add tests for failure cases

--- tests/unit/jobserver/management/commands/test_check_rap_api_status.py
+++ tests/unit/jobserver/management/commands/test_check_rap_api_status.py
@@ -1,6 +1,8 @@
+import pytest
 import responses
 import responses.matchers
 from django.conf import settings
+from django.core.management.base import CommandError
 
 from jobserver.management.commands.check_rap_api_status import Command as cd
 
@@ -21,3 +23,32 @@ def test_check_rap_api_status():
 
         result = cd.check_rap_api_status(url, api_token)
         assert result == b'{"method": "GET"}'
+
+
+def test_check_rap_api_status_bad_token():
+    with responses.RequestsMock() as rsps:
+        # default rap api url
+        url = settings.RAP_API_ENDPOINT
+        api_token = "badtoken"
+
+        rsps.add(
+            responses.GET,
+            f"{url}/backend/status",
+            body=b'{"method": "GET"}',
+            match=[responses.matchers.header_matcher({"Authorization": api_token})],
+            status=401,
+        )
+
+        with pytest.raises(CommandError, match="returned an error"):
+            cd.check_rap_api_status(url, api_token)
+
+
+def test_check_rap_api_status_not_available():
+    # Ask responses to intercept requests, but do not define any
+    with responses.RequestsMock():
+        # default rap api url
+        url = settings.RAP_API_ENDPOINT
+        api_token = settings.RAP_API_TOKEN
+
+        with pytest.raises(CommandError, match="not available"):
+            cd.check_rap_api_status(url, api_token)
