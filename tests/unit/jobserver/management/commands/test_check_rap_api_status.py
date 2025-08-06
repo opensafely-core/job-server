54fd262ff302b0664ccd36e1052358dd290b0f63
Tom Ward <tomward@fmail.co.uk>
2025-08-06 12:38:59 +0100

    Replace fixtures with globvars

--- tests/unit/jobserver/management/commands/test_check_rap_api_status.py
+++ tests/unit/jobserver/management/commands/test_check_rap_api_status.py
@@ -7,80 +7,70 @@ from django.core.management import CommandError, call_command
 from jobserver.management.commands.check_rap_api_status import Command as cd
 
 
-@pytest.fixture
-def base_url():
-    return settings.RAP_API_BASE_URL
+TEST_BASE_URL = settings.RAP_API_BASE_URL
+TEST_STATUS_URL = f"{TEST_BASE_URL}/backend/status"
+TEST_API_TOKEN = settings.RAP_API_TOKEN
+TEST_RESPONSE_BODY = b'{"flags":{"test":{"paused":{"v":null,"ts":"2025-05-09T15:05:09.010195Z"},"last-seen-at":{"v":"2025-07-18T09:29:20.504634+00:00","ts":"2025-07-18T09:29:20.504842Z"}}}}'
 
 
-@pytest.fixture
-def status_url(base_url):
-    return f"{base_url}/backend/status"
-
-
-@pytest.fixture
-def api_token():
-    return settings.RAP_API_TOKEN
-
-
-@pytest.fixture
-def response_body():
-    yield b'{"flags":{"test":{"paused":{"v":null,"ts":"2025-05-09T15:05:09.010195Z"},"last-seen-at":{"v":"2025-07-18T09:29:20.504634+00:00","ts":"2025-07-18T09:29:20.504842Z"}}}}'
-
-
-def test_check_rap_api_status(base_url, status_url, api_token, response_body):
+def test_check_rap_api_status():
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.GET,
-            status_url,
-            body=response_body,
-            match=[responses.matchers.header_matcher({"Authorization": api_token})],
+            TEST_STATUS_URL,
+            body=TEST_RESPONSE_BODY,
+            match=[
+                responses.matchers.header_matcher({"Authorization": TEST_API_TOKEN})
+            ],
             status=200,
         )
 
-        result = cd.check_rap_api_status(base_url, api_token)
-        assert result == response_body
+        result = cd.check_rap_api_status(TEST_BASE_URL, TEST_API_TOKEN)
+        assert result == TEST_RESPONSE_BODY
 
 
-def test_check_rap_api_status_bad_token(base_url, status_url):
+def test_check_rap_api_status_bad_token():
     with responses.RequestsMock() as rsps:
         api_token_bad = "badtoken"
 
         rsps.add(
             responses.GET,
-            status_url,
+            TEST_STATUS_URL,
             match=[responses.matchers.header_matcher({"Authorization": api_token_bad})],
             status=401,
         )
 
         with pytest.raises(CommandError, match="returned an error"):
-            cd.check_rap_api_status(base_url, api_token_bad)
+            cd.check_rap_api_status(TEST_BASE_URL, api_token_bad)
 
 
-def test_check_rap_api_status_not_available(base_url, status_url, api_token):
+def test_check_rap_api_status_not_available():
     # Ask responses to intercept requests, but do not define any
     with responses.RequestsMock():
         with pytest.raises(CommandError, match="not available"):
-            cd.check_rap_api_status(base_url, api_token)
+            cd.check_rap_api_status(TEST_BASE_URL, TEST_API_TOKEN)
 
 
-def test_command(base_url, status_url, api_token, response_body, log_output):
+def test_command(log_output):
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.GET,
-            status_url,
-            body=response_body,
-            match=[responses.matchers.header_matcher({"Authorization": api_token})],
+            TEST_STATUS_URL,
+            body=TEST_RESPONSE_BODY,
+            match=[
+                responses.matchers.header_matcher({"Authorization": TEST_API_TOKEN})
+            ],
             status=200,
         )
         call_command("check_rap_api_status")
 
         assert log_output.entries[0] == {
-            "event": response_body,
+            "event": TEST_RESPONSE_BODY,
             "log_level": "info",
         }
 
 
-def test_command_error(base_url, status_url, api_token, log_output):
+def test_command_error(log_output):
     with responses.RequestsMock():
         call_command("check_rap_api_status")
 
