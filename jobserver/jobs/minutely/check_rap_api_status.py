76781dc262d129bf7a046737e5cf01c8b03905d6
Becky Smith <becky.smith@thedatalab.org>
2025-08-21 16:21:46 +0100

    Set more sensible monitor config for minutely cron

--- jobserver/jobs/minutely/check_rap_api_status.py
+++ jobserver/jobs/minutely/check_rap_api_status.py
@@ -9,7 +9,14 @@ class Job(MinutelyJob):
     help = "Run the check_rap_api_status management command"  # noqa: A003
 
     @monitor(
-        monitor_slug="check_rap_api_status", monitor_config=monitor_config("* * * * *")
+        monitor_slug="check_rap_api_status",
+        monitor_config=monitor_config(
+            "* * * * *",
+            checkin_margin=1,
+            max_runtime=1,
+            failure_issue_threshold=10,
+            recovery_threshold=1,
+        ),
     )
     def execute(self):
         call_command("check_rap_api_status")
