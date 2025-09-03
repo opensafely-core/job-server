3ce0aed1035dc4f212d09bfb16d10d2725feec57
Tom Ward <tomward@fmail.co.uk>
2025-09-03 17:16:31 +0100

    Add simple rap/status management command
    * just log the results, don't process

---
+++ jobserver/management/commands/rap_status.py
@@ -0,0 +1,41 @@
+"""Management command to update the status of a RAP's Jobs via the RAP API."""
+
+import json
+
+import structlog
+from django.core.management.base import BaseCommand
+
+from jobserver import rap_api
+
+
+class Command(BaseCommand):
+    """Management command to update the status of a RAP's Jobs via the RAP API."""
+
+    help = "Update RAP's jobs status via the RAP API."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "rap_ids",
+            nargs="+",
+            type=str,
+            help="IDs of the RAPs to update.",
+        )
+
+    def handle(self, *args, **options):
+        logger = structlog.get_logger(__name__)
+        try:
+            json_response = json.loads(
+                rap_api.status(
+                    options["rap_ids"],
+                )
+            )
+
+            for job in json_response["jobs"]:
+                logger.info(
+                    f"RAP: {job['rap_id']} Job: {job['identifier']} Status: {job['status']}"
+                )
+            for unrecognised_rap_id in json_response["unrecognised_rap_ids"]:
+                logger.info(f"Unrecognised RAP id: {unrecognised_rap_id}")
+
+        except Exception as exc:
+            logger.error(exc)
---
+++ tests/unit/jobserver/management/commands/test_rap_status.py
@@ -0,0 +1,47 @@
+from django.core.management import call_command
+
+
+def test_command(monkeypatch, log_output):
+    test_response_body = b'{"jobs": [{"identifier": "abc123", "rap_id": "abcdefgi12345678", "status": "succeeded"}], "unrecognised_rap_ids": []}'
+    test_log_output = "RAP: abcdefgi12345678 Job: abc123 Status: succeeded"
+
+    monkeypatch.setattr(
+        "jobserver.rap_api.status",
+        lambda rap_ids: test_response_body,
+    )
+
+    call_command("rap_status", "abcdefgi12345678")
+
+    assert log_output.entries[0] == {
+        "event": test_log_output,
+        "log_level": "info",
+    }
+
+
+def test_command_unrecognised(monkeypatch, log_output):
+    test_response_body = b'{"jobs": [], "unrecognised_rap_ids": ["87654321ihgfedcba"]}'
+    test_log_output = "Unrecognised RAP id: 87654321ihgfedcba"
+
+    monkeypatch.setattr(
+        "jobserver.rap_api.status",
+        lambda rap_ids: test_response_body,
+    )
+
+    call_command("rap_status", "87654321ihgfedcba")
+
+    assert log_output.entries[0] == {
+        "event": test_log_output,
+        "log_level": "info",
+    }
+
+
+def test_command_error(monkeypatch, log_output):
+    def fake_cancel(rap_ids):
+        raise Exception("something went wrong")
+
+    monkeypatch.setattr("jobserver.rap_api.status", fake_cancel)
+
+    call_command("rap_status", "abcdefgi12345678")
+
+    assert "error" == log_output.entries[0]["log_level"]
+    assert "something went wrong" in str(log_output.entries[0]["event"])
