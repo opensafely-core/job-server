1c857cdb7096f633be463cb312e915c437f0ff69
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-09-09 03:38:29 +0100

    Test `update_backend_state` functions as expected

--- jobserver/management/commands/check_rap_api_status.py
+++ jobserver/management/commands/check_rap_api_status.py
@@ -17,25 +17,23 @@ class Command(BaseCommand):
             logger.error(exc)
 
     def update_backend_state(backend, request):
-        # Retrieve backend state sent up from job-runner. We might rename the header this is
-        # passed in at some point but for now this is good enough.
-        flags = rap_api.backend_status().get("backends", "")
+        # Retrieve the response gotten from the jobrunner endpoint
+        flags = rap_api.backend_status().get("backends", [])
 
-        # if not flags:  # this line should never be hit because we have a default dict compared with the old way
-        #     return
+        if not flags:
+            return
 
         jobrunner_state = flags[0]
 
         # Record the time we're told this backend was last seen alive, for availability
         # reporting purposes
-        if last_seen_at_flag := jobrunner_state.get("last_seen", ""):
+        if last_seen_at_flag := jobrunner_state.get("last_seen"):
             last_seen_at_dt = datetime.datetime.fromisoformat(last_seen_at_flag)
             Stats.objects.update_or_create(
                 backend=backend,
                 url=request.path,
                 defaults={"api_last_seen": last_seen_at_dt},
             )
-        else:
-            return
+
         backend.jobrunner_rap_api_state = jobrunner_state
         backend.save(update_fields=["jobrunner_rap_api_state"])
