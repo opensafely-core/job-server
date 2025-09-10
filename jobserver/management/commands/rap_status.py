5171cd84b38d7aac60a986239fea95bd76fb2dcf
Tom Ward <tomward@fmail.co.uk>
2025-09-10 16:22:23 +0100

    Port job create/update logic from jobserver.api.jobs
    * trying to make as few changes as possible, whilst using
      the rap/status API response
    * write lots of tests, based on the tests in test_jobs.py
      e.g. test_jobapiupdate_all_existing
    * log job identifier when deleting, because the id won't
      be very useful once we've deleted it from the db!

--- jobserver/management/commands/rap_status.py
+++ jobserver/management/commands/rap_status.py
@@ -4,9 +4,12 @@ import structlog
 from django.core.management.base import BaseCommand
 
 from jobserver import rap_api
+from jobserver.models import JobRequest
 
 
 logger = structlog.get_logger(__name__)
+
+COMPLETED_STATES = {"failed", "succeeded"}
 
 
 class Command(BaseCommand):
@@ -27,13 +30,111 @@ class Command(BaseCommand):
             json_response = rap_api.status(
                 options["rap_ids"],
             )
+            job_requests = JobRequest.objects.filter(identifier__in=options["rap_ids"])
+            job_request_lut = {jr.identifier: jr for jr in job_requests}
 
-            for job in json_response["jobs"]:
-                logger.info(
-                    f"RAP: {job['rap_id']} Job: {job['identifier']} Status: {job['status']}"
+            created_job_ids = []
+            updated_job_ids = []
+            deleted_identifiers = []
+
+            for rap_id in options["rap_ids"]:
+                # TODO: move this out of the management command!!
+
+                if rap_id in json_response["unrecognised_rap_ids"]:
+                    continue
+
+                # get the JobRequest for this identifier
+                job_request = job_request_lut.get(rap_id)
+
+                # This could happen if the management command is given a rap_id which
+                # does not exist on job-server but does exist on the controller.
+                if job_request is None:
+                    logger.info(f"Job-server does not recognise RAP id: {rap_id}")
+                    continue
+
+                # bind the job request ID to further logs so looking them up
+                # in the UI is easier
+                structlog.contextvars.bind_contextvars(job_request=job_request.id)
+
+                jobs_from_api = [
+                    j for j in json_response["jobs"] if j.get("rap_id") == rap_id
+                ]
+
+                database_jobs = job_request.jobs.all()
+
+                # get the current Jobs for the JobRequest, keyed on their identifier
+                jobs_by_identifier = {j.identifier: j for j in database_jobs}
+
+                payload_identifiers = {j["identifier"] for j in jobs_from_api}
+
+                # delete local jobs not in the payload
+                identifiers_to_delete = (
+                    set(jobs_by_identifier.keys()) - payload_identifiers
                 )
-            for unrecognised_rap_id in json_response["unrecognised_rap_ids"]:
-                logger.info(f"Unrecognised RAP id: {unrecognised_rap_id}")
+                if identifiers_to_delete:
+                    job_request.jobs.filter(
+                        identifier__in=identifiers_to_delete
+                    ).delete()
+                    for identifier in identifiers_to_delete:
+                        deleted_identifiers.append(str(identifier))
+
+                for job_from_api in jobs_from_api:
+                    logger.info(
+                        f"RAP: {job_from_api['rap_id']} Job: {job_from_api['identifier']} Status: {job_from_api['status']}"
+                    )
+
+                    # Remove rap_id from the data, it's going to be set by
+                    # creating/updating Job instances via the JobRequest instances
+                    # related Jobs manager (ie job_request.jobs)
+                    # Also remove some other values which we don't currently store in Job
+                    superfluous_keys = ["rap_id", "backend", "requires_db"]
+
+                    for superfluous_key in superfluous_keys:
+                        job_from_api.pop(superfluous_key, None)
+
+                    job_from_db, created = job_request.jobs.get_or_create(
+                        identifier=job_from_api["identifier"],
+                        defaults={**job_from_api},
+                    )
+
+                    if created:
+                        created_job_ids.append(str(job_from_db.id))
+                        # For newly created jobs we can't tell if they've just
+                        # transitioned to completed so we assume they have to avoid
+                        # missing notifications
+                        newly_completed = job_from_db.status in COMPLETED_STATES
+                    else:
+                        updated_job_ids.append(str(job_from_db.id))
+
+                        newly_completed = (
+                            job_from_db.status not in COMPLETED_STATES
+                            and job_from_api["status"] in COMPLETED_STATES
+                        )
+
+                        # update Job "manually" so we can make the check above for
+                        # status transition
+                        for key, value in job_from_api.items():
+                            setattr(job_from_db, key, value)
+
+                    job_from_db.save()
+
+                    if newly_completed:
+                        job_from_db.refresh_from_db()
+                        logger.info(f"{job_from_db.identifier} newly completed")
+                        # TODO: port this to here!
+                        # handle_job_notifications(job_request, job_from_db)
+
+            # TODO: should we log ids or identifiers here? ids are not so helpful
+            logger.info(
+                "Created, updated or deleted Jobs",
+                created_job_ids=",".join(created_job_ids),
+                updated_job_ids=",".join(updated_job_ids),
+                deleted_job_identifiers=",".join(deleted_identifiers),
+            )
+            if json_response["unrecognised_rap_ids"]:
+                logger.info(
+                    f"Unrecognised RAP ids: {','.join(json_response['unrecognised_rap_ids'])}"
+                )
 
         except Exception as exc:
             logger.error(exc)
--- tests/unit/jobserver/management/commands/test_rap_status.py
+++ tests/unit/jobserver/management/commands/test_rap_status.py
@@ -1,54 +1,520 @@
+from unittest.mock import patch
+
+import pytest
 from django.core.management import call_command
+from django.utils import timezone
+
+from jobserver.models import Job
+from tests.factories import (
+    JobFactory,
+    JobRequestFactory,
+)
+from tests.utils import minutes_ago, seconds_ago
 
 
-def test_command(monkeypatch, log_output):
+@patch("jobserver.rap_api.status")
+def test_update_job_simple(mock_rap_api_status, log_output):
+    job_request = JobRequestFactory()
+
+    now = timezone.now()
+
+    assert Job.objects.count() == 0
+
+    job1 = JobFactory.create(
+        job_request=job_request,
+        started_at=None,
+        status="pending",
+        completed_at=None,
+    )
+    assert Job.objects.count() == 1
+
     test_response_json = {
         "jobs": [
             {
-                "identifier": "abc123",
-                "rap_id": "abcdefgi12345678",
+                "identifier": job1.identifier,
+                "rap_id": job_request.identifier,
+                "action": "test-action1",
+                "backend": "test",
+                "run_command": "do-research",
                 "status": "succeeded",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": seconds_ago(now, 30),
+                "metrics": {"cpu_peak": 99},
+            },
+        ],
+        "unrecognised_rap_ids": [],
+    }
+    mock_rap_api_status.return_value = test_response_json
+
+    call_command("rap_status", job_request.identifier)
+
+    # we shouldn't have a different number of jobs
+    jobs = Job.objects.all()
+    assert len(jobs) == 1
+
+    # check our jobs look as expected
+    updated_job1 = jobs[0]
+
+    # succeeded
+    assert updated_job1.identifier == job1.identifier
+    assert updated_job1.started_at == minutes_ago(now, 1)
+    assert updated_job1.updated_at == now
+    assert updated_job1.completed_at == seconds_ago(now, 30)
+    assert updated_job1.metrics == {"cpu_peak": 99}
+    assert (
+        log_output.entries[0]["event"]
+        == f"RAP: {job_request.identifier} Job: {job1.identifier} Status: succeeded"
+    )
+    assert log_output.entries[1]["event"] == f"{job1.identifier} newly completed"
+
+    assert log_output.entries[-1]["event"] == "Created, updated or deleted Jobs"
+    assert log_output.entries[-1]["updated_job_ids"] == str(job1.id)
+
+
+@pytest.mark.parametrize("pre_existing", [True, False])
+@patch("jobserver.rap_api.status")
+def test_update_job_multiple(mock_rap_api_status, log_output, pre_existing):
+    job_request = JobRequestFactory()
+
+    now = timezone.now()
+
+    assert Job.objects.count() == 0
+
+    if pre_existing:
+        # 3 pending jobs already exist
+        job1, job2, job3 = JobFactory.create_batch(
+            3,
+            job_request=job_request,
+            started_at=None,
+            status="pending",
+            completed_at=None,
+        )
+        job1.identifier = "job1"
+        job1.save()
+
+        job2.identifier = "job2"
+        job2.save()
+
+        job3.identifier = "job3"
+        job3.save()
+
+        assert Job.objects.count() == 3
+
+    test_response_json = {
+        "jobs": [
+            {
+                "identifier": "job1",
+                "rap_id": job_request.identifier,
+                "action": "test-action1",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "succeeded",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": seconds_ago(now, 30),
+                "metrics": {"cpu_peak": 99},
+            },
+            {
+                "identifier": "job2",
+                "rap_id": job_request.identifier,
+                "action": "test-action2",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "running",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": None,
+            },
+            {
+                "identifier": "job3",
+                "rap_id": job_request.identifier,
+                "action": "test-action3",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "pending",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": None,
+                "updated_at": now,
+                "completed_at": None,
+            },
+        ],
+        "unrecognised_rap_ids": [],
+    }
+    mock_rap_api_status.return_value = test_response_json
+
+    call_command("rap_status", job_request.identifier)
+
+    # we shouldn't have a different number of jobs
+    jobs = Job.objects.all()
+    assert len(jobs) == 3
+
+    # check our jobs look as expected
+    job1, job2, job3 = jobs
+
+    # succeeded
+    assert job1.identifier == "job1"
+    assert job1.started_at == minutes_ago(now, 1)
+    assert job1.updated_at == now
+    assert job1.completed_at == seconds_ago(now, 30)
+    assert job1.metrics == {"cpu_peak": 99}
+    assert (
+        log_output.entries[0]["event"]
+        == f"RAP: {job_request.identifier} Job: job1 Status: succeeded"
+    )
+    assert log_output.entries[1]["event"] == f"{job1.identifier} newly completed"
+
+    # running
+    assert job2.identifier == "job2"
+    assert job2.started_at == minutes_ago(now, 1)
+    assert job2.updated_at == now
+    assert job2.completed_at is None
+    assert (
+        log_output.entries[2]["event"]
+        == f"RAP: {job_request.identifier} Job: job2 Status: running"
+    )
+
+    # pending
+    assert job3.identifier == "job3"
+    assert job3.started_at is None
+    assert job3.updated_at == now
+    assert job3.completed_at is None
+    assert (
+        log_output.entries[3]["event"]
+        == f"RAP: {job_request.identifier} Job: job3 Status: pending"
+    )
+
+    assert log_output.entries[4]["event"] == "Created, updated or deleted Jobs"
+    if pre_existing:
+        assert (
+            log_output.entries[4]["updated_job_ids"] == f"{job1.id},{job2.id},{job3.id}"
+        )
+    else:
+        assert (
+            log_output.entries[4]["created_job_ids"] == f"{job1.id},{job2.id},{job3.id}"
+        )
+
+
+@patch("jobserver.rap_api.status")
+def test_update_jobs_multiple_job_requests(mock_rap_api_status, log_output):
+    job_request1 = JobRequestFactory()
+    job_request2 = JobRequestFactory()
+
+    now = timezone.now()
+
+    assert Job.objects.count() == 0
+
+    job1, job2 = JobFactory.create_batch(
+        2,
+        job_request=job_request1,
+        started_at=None,
+        status="pending",
+        completed_at=None,
+    )
+    job3, job4 = JobFactory.create_batch(
+        2,
+        job_request=job_request2,
+        started_at=None,
+        status="pending",
+        completed_at=None,
+    )
+    assert Job.objects.count() == 4
+
+    test_response_json = {
+        "jobs": [
+            {
+                "identifier": job1.identifier,
+                "rap_id": job_request1.identifier,
+                "action": "test-action1",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "succeeded",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": seconds_ago(now, 30),
+                "metrics": {"cpu_peak": 99},
+            },
+            {
+                "identifier": job2.identifier,
+                "rap_id": job_request1.identifier,
+                "action": "test-action2",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "running",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": None,
+            },
+            {
+                "identifier": job3.identifier,
+                "rap_id": job_request2.identifier,
+                "action": "test-action2",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "running",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": None,
+            },
+            {
+                "identifier": job4.identifier,
+                "rap_id": job_request2.identifier,
+                "action": "test-action3",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "pending",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": None,
+                "updated_at": now,
+                "completed_at": None,
+            },
+        ],
+        "unrecognised_rap_ids": [],
+    }
+    mock_rap_api_status.return_value = test_response_json
+
+    call_command("rap_status", [job_request1.identifier, job_request2.identifier])
+
+    # Check the command worked overall
+    assert log_output.entries[-1]["event"] == "Created, updated or deleted Jobs"
+    assert (
+        log_output.entries[-1]["updated_job_ids"]
+        == f"{job1.id},{job2.id},{job3.id},{job4.id}"
+    )
+
+    # we shouldn't have a different number of jobs
+    jobs = Job.objects.all()
+    assert len(jobs) == 4
+
+    # Check the details of the jobs
+    updated_job1, updated_job2, updated_job3, updated_job4 = jobs
+
+    # job_request1 job succeeded
+    assert updated_job1.identifier == job1.identifier
+    assert updated_job1.started_at == minutes_ago(now, 1)
+    assert updated_job1.updated_at == now
+    assert updated_job1.completed_at == seconds_ago(now, 30)
+    assert updated_job1.metrics == {"cpu_peak": 99}
+    assert (
+        log_output.entries[0]["event"]
+        == f"RAP: {job_request1.identifier} Job: {job1.identifier} Status: succeeded"
+    )
+    assert log_output.entries[1]["event"] == f"{job1.identifier} newly completed"
+
+    # job_request1 job running
+    assert updated_job2.identifier == job2.identifier
+    assert updated_job2.started_at == minutes_ago(now, 1)
+    assert updated_job2.updated_at == now
+    assert updated_job2.completed_at is None
+    assert (
+        log_output.entries[2]["event"]
+        == f"RAP: {job_request1.identifier} Job: {job2.identifier} Status: running"
+    )
+
+    # job_request2 job running
+    assert updated_job3.identifier == job3.identifier
+    assert updated_job3.started_at == minutes_ago(now, 1)
+    assert updated_job3.updated_at == now
+    assert updated_job3.completed_at is None
+    assert (
+        log_output.entries[3]["event"]
+        == f"RAP: {job_request2.identifier} Job: {job3.identifier} Status: running"
+    )
+
+    # job_request2 job pending
+    assert updated_job4.identifier == job4.identifier
+    assert updated_job4.started_at is None
+    assert updated_job4.updated_at == now
+    assert updated_job4.completed_at is None
+    assert (
+        log_output.entries[4]["event"]
+        == f"RAP: {job_request2.identifier} Job: {job4.identifier} Status: pending"
+    )
+
+
+@patch("jobserver.rap_api.status")
+def test_delete_jobs(mock_rap_api_status, log_output):
+    job_request = JobRequestFactory()
+
+    now = timezone.now()
+
+    assert Job.objects.count() == 0
+
+    jobs = JobFactory.create_batch(
+        2,
+        job_request=job_request,
+        started_at=None,
+        status="pending",
+        completed_at=None,
+    )
+
+    # rap/status only returns one job of the two created
+    test_response_json = {
+        "jobs": [
+            {
+                "identifier": jobs[0].identifier,
+                "rap_id": job_request.identifier,
+                "action": "test-action1",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "succeeded",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": seconds_ago(now, 30),
+                "metrics": {"cpu_peak": 99},
             }
         ],
         "unrecognised_rap_ids": [],
     }
-    test_log_output = "RAP: abcdefgi12345678 Job: abc123 Status: succeeded"
 
-    monkeypatch.setattr(
-        "jobserver.rap_api.status",
-        lambda rap_ids: test_response_json,
+    identifier_to_delete = jobs[1].identifier
+
+    assert Job.objects.count() == 2
+
+    mock_rap_api_status.return_value = test_response_json
+
+    call_command("rap_status", job_request.identifier)
+
+    # The second job should have been deleted
+    jobs = Job.objects.all()
+    assert len(jobs) == 1
+
+    assert log_output.entries[-1]["event"] == "Created, updated or deleted Jobs"
+    assert log_output.entries[-1]["updated_job_ids"] == str(jobs[0].id)
+    assert log_output.entries[-1]["deleted_job_identifiers"] == identifier_to_delete
+
+
+@pytest.mark.slow_test
+@patch("jobserver.rap_api.status")
+def test_update_lots_of_jobs(mock_rap_api_status, log_output):
+    job_request = JobRequestFactory()
+
+    now = timezone.now()
+
+    assert Job.objects.count() == 0
+
+    jobs = JobFactory.create_batch(
+        5000,
+        job_request=job_request,
+        started_at=None,
+        status="pending",
+        completed_at=None,
     )
 
-    call_command("rap_status", "abcdefgi12345678")
-
-    assert log_output.entries[0] == {
-        "event": test_log_output,
-        "log_level": "info",
+    test_response_json = {
+        "jobs": [],
+        "unrecognised_rap_ids": [],
     }
 
+    for job in jobs:
+        test_response_json["jobs"].append(
+            {
+                "identifier": job.identifier,
+                "rap_id": job_request.identifier,
+                "action": "test-action1",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "succeeded",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": seconds_ago(now, 30),
+                "metrics": {"cpu_peak": 99},
+            }
+        )
 
-def test_command_unrecognised(monkeypatch, log_output):
+    assert Job.objects.count() == 5000
+
+    mock_rap_api_status.return_value = test_response_json
+
+    call_command("rap_status", job_request.identifier)
+
+    # we shouldn't have a different number of jobs
+    jobs = Job.objects.all()
+    assert len(jobs) == 5000
+
+    assert log_output.entries[-1]["event"] == "Created, updated or deleted Jobs"
+
+
+@patch("jobserver.rap_api.status")
+def test_command_unrecognised_controller(mock_rap_api_status, log_output):
     test_response_json = {"jobs": [], "unrecognised_rap_ids": ["87654321ihgfedcba"]}
-    test_log_output = "Unrecognised RAP id: 87654321ihgfedcba"
 
-    monkeypatch.setattr(
-        "jobserver.rap_api.status",
-        lambda rap_ids: test_response_json,
-    )
+    mock_rap_api_status.return_value = test_response_json
 
     call_command("rap_status", "87654321ihgfedcba")
 
-    assert log_output.entries[0] == {
-        "event": test_log_output,
-        "log_level": "info",
+    assert log_output.entries[0]["event"] == "Created, updated or deleted Jobs"
+    assert log_output.entries[0]["updated_job_ids"] == ""
+    assert log_output.entries[0]["created_job_ids"] == ""
+    assert log_output.entries[0]["deleted_job_identifiers"] == ""
+    assert log_output.entries[1]["event"] == "Unrecognised RAP ids: 87654321ihgfedcba"
+
+
+@patch("jobserver.rap_api.status")
+def test_command_unrecognised_jobserver(mock_rap_api_status, log_output):
+    now = timezone.now()
+
+    test_response_json = {
+        "jobs": [
+            {
+                "identifier": "job1",
+                "rap_id": "job_request1",
+                "action": "test-action1",
+                "backend": "test",
+                "run_command": "do-research",
+                "status": "succeeded",
+                "status_code": "",
+                "status_message": "",
+                "created_at": minutes_ago(now, 2),
+                "started_at": minutes_ago(now, 1),
+                "updated_at": now,
+                "completed_at": seconds_ago(now, 30),
+                "metrics": {"cpu_peak": 99},
+            },
+        ],
+        "unrecognised_rap_ids": [],
     }
 
+    mock_rap_api_status.return_value = test_response_json
 
-def test_command_error(monkeypatch, log_output):
-    def fake_cancel(rap_ids):
-        raise Exception("something went wrong")
+    call_command("rap_status", "job1")
 
-    monkeypatch.setattr("jobserver.rap_api.status", fake_cancel)
+    assert (
+        log_output.entries[0]["event"] == "Job-server does not recognise RAP id: job1"
+    )
+
+
+@patch("jobserver.rap_api.status")
+def test_command_error(mock_rap_api_status, log_output):
+    mock_rap_api_status.side_effect = Exception("something went wrong")
 
     call_command("rap_status", "abcdefgi12345678")
 
