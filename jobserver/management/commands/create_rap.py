f1e7a9481478126aa22a43f2900747aea45b2c7e
Becky Smith <becky.smith@thedatalab.org>
2025-09-29 14:57:12 +0100

    Extract RAP creation to a method on the JobRequest model
    We're going to want to do all of this in the job request creation
    view, so extract it to a method that we can reuse.

--- jobserver/management/commands/create_rap.py
+++ jobserver/management/commands/create_rap.py
@@ -3,9 +3,8 @@
 import structlog
 from django.core.management.base import BaseCommand
 
-from jobserver import rap_api
 from jobserver.github import _get_github_api
-from jobserver.models import Backend, JobRequestStatus, User, Workspace
+from jobserver.models import Backend, User, Workspace
 from jobserver.pipeline_config import get_codelists_status, get_project
 
 
@@ -85,39 +84,4 @@ class Command(BaseCommand):
             force_run_dependencies=options["force_run_dependencies"],
         )
 
-        try:
-            json_response = rap_api.create(job_request)
-            logger.info(json_response)
-
-            if json_response["count"] > 0:
-                # New jobs have been created, status is Pending
-                job_request.update_status(JobRequestStatus.PENDING)
-            else:
-                # Nothing to do, update status and record response details as status message
-                job_request.update_status(
-                    JobRequestStatus.NOTHING_TO_DO, json_response["details"]
-                )
-        except rap_api.RapAPIResponseError as exc:
-            logger.error(exc)
-            # Failed in creating jobs. Set status to Failed, record exc body as status message
-            job_request.update_status(JobRequestStatus.FAILED, exc.body["details"])
-        except rap_api.RapAPIRequestError as exc:
-            # Encountered some unhandled error whilst contacting the RAP API to creating jobs.
-            # This is most likely due to some disconnect between job-server and controller, and jobs
-            # could have been created on the controller. We mark job requests status as unknown, and
-            # allow the code that polls the controller for job status to request updates for it again.
-            logger.error(exc)
-            job_request.update_status(JobRequestStatus.UNKNOWN)
-        except Exception as exc:
-            # Any other exception just fails the whole job request. We show an unknown error to avoid
-            # leaking the content of the exception to the user.
-            logger.error(exc)
-            job_request.update_status(
-                JobRequestStatus.FAILED, "Unknown error creating jobs"
-            )
-
-        logger.info(
-            "Job request created",
-            rap_id=job_request.identifier,
-            status=job_request.jobs_status,
-        )
+        job_request.request_rap_creation()
