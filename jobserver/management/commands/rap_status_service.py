8f2f9bede025b8c512ee35418997edb9f8793911
Becky Smith <becky.smith@thedatalab.org>
2025-10-10 15:19:08 +0100

    Use a run_fn for the status update loop and move error handling
    Using a run_fn (which by default just returns True) means that we can
    test a finite number of loops.
    
    Move the try/except inside the while loop so that a caught exception
    doesn't cause the whole loop to stop. We want to instead catch, log and
    keep the loop going.

--- jobserver/management/commands/rap_status_service.py
+++ jobserver/management/commands/rap_status_service.py
@@ -1,5 +1,6 @@
 """Management command to continually update the status of all active Jobs via the RAP API."""
 
+import argparse
 import time
 
 import structlog
@@ -17,11 +18,17 @@ class Command(BaseCommand):
 
     help = "Update active RAPs' jobs status via the RAP API."
 
+    def add_arguments(self, parser):
+        # In production, we want this loop to run forever. Using a
+        # function means that we can test it on a finite number of loops.
+        parser.add_argument("--run-fn", default=lambda: True, help=argparse.SUPPRESS)
+
     def handle(self, *args, **options):
-        try:
-            while True:
+        run_fn = options["run_fn"]
+        while run_fn():
+            try:
                 active_job_requests_ids = get_active_job_request_ids()
                 rap_status_update(active_job_requests_ids)
                 time.sleep(settings.RAP_API_POLL_INTERVAL)
-        except Exception as exc:
-            logger.error(exc)
+            except Exception as exc:
+                logger.error(exc)
