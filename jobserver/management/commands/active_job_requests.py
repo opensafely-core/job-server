fb3dace6f8e6c42fcdef45ce940bdeafc840ab1c
Becky Smith <becky.smith@thedatalab.org>
2025-10-10 18:41:04 +0100

    Add tests for active_job_requests management command

--- jobserver/management/commands/active_job_requests.py
+++ jobserver/management/commands/active_job_requests.py
@@ -14,6 +14,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            logger.info(get_active_job_request_ids())
+            logger.info("Active job requests", rap_ids=get_active_job_request_ids())
         except Exception as exc:
             logger.error(exc)
