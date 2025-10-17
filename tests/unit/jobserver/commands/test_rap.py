6b492989f34d9e4d9b7169aa774fed0afb8a58a4
Becky Smith <becky.smith@thedatalab.org>
2025-10-17 12:53:35 +0100

    Move rap_status_response_factory to factories so we can reuse it

--- tests/unit/jobserver/commands/test_rap.py
+++ tests/unit/jobserver/commands/test_rap.py
@@ -15,6 +15,7 @@ from tests.factories import (
     JobFactory,
     JobRequestFactory,
     WorkspaceFactory,
+    rap_status_response_factory,
 )
 from tests.utils import minutes_ago, seconds_ago
 
@@ -104,46 +105,6 @@ def check_job_request_status(rap_id, expected_status):
     assert JobRequestStatus(job_request.jobs_status) == expected_status
     # return the refreshed job request for tests that need to use it
     return job_request
-
-
-def isoformat_datetime(dt):
-    if dt is not None:
-        dt = dt.isoformat()
-    return dt
-
-
-def rap_status_response_factory(jobs, unrecognised_rap_ids, now):
-    jobs_response = []
-    for job in jobs:
-        jobs_response.append(
-            {
-                "identifier": job.get("identifier", "identifier-0"),
-                "rap_id": job.get("rap_id", "rap-identifier-0"),
-                "action": job.get("action", "test-action1"),
-                "backend": job.get("backend", "test"),
-                "run_command": job.get("run_command", "do-research"),
-                "requires_db": job.get("requires_db", "false"),
-                "status": job.get("status", "succeeded"),
-                "status_code": "",
-                "status_message": "",
-                # datetimes in the json response from the RAP API are received as isoformat strings
-                "created_at": isoformat_datetime(
-                    job.get("created_at", minutes_ago(now, 2))
-                ),
-                "started_at": isoformat_datetime(
-                    job.get("started_at", minutes_ago(now, 1))
-                ),
-                "updated_at": now.isoformat(),
-                "completed_at": isoformat_datetime(
-                    job.get("completed_at", seconds_ago(now, 30))
-                ),
-                "metrics": {"cpu_peak": 99},
-            }
-        )
-    return {
-        "jobs": jobs_response,
-        "unrecognised_rap_ids": unrecognised_rap_ids,
-    }
 
 
 @patch("jobserver.rap_api.status")
