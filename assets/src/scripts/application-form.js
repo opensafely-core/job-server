6d971632a1de0d88d70af2a907782531eeb11cd3
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-10-29 13:34:18 +0000

    Fix biome warnings for return statements

--- assets/src/scripts/application-form.js
+++ assets/src/scripts/application-form.js
@@ -2,7 +2,7 @@ const content = document?.getElementById("applicationForm");
 const links = content?.getElementsByTagName("a");
 
 if (links?.length) {
-  [...links].map((link) => {
+  links.forEach((link) => {
     if (link.hostname !== window.location.hostname) {
       link.target = "_blank";
       link.rel = "noopener noreferrer";
