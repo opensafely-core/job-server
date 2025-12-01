71e33c84c9fea2ed7a91669277c573d06fab550c
Becky Smith <becky.smith@thedatalab.org>
2025-12-01 15:27:14 +0000

    Make the ndoo module more generic
    We expect there to eventually be multiple modules that populate the
    RAP API analysis_scope parameter. This makes the ndoo module more
    generic, so that future additional modules can use the same interface.

--- jobserver/permissions/population_permissions/ndoo.py
+++ jobserver/permissions/population_permissions/ndoo.py
@@ -17,6 +17,8 @@
 # TODO: Complete this list! Currently just includes a few of projects for example purposes
 # Note: projects before 156 cannot be re-run, so they are not included here (several projects from 156 onwards are
 # approved continuations of previous projects)
+ANALYSIS_SCOPE_KEY = "include_ndoo"
+
 PROJECTS_WITH_NDOO_PERMISSION = {
     156,  # https://jobs.opensafely.org/investigating-events-following-sars-cov-2-infection-project-continuation-of-approved-project-no-12/
     157,  # https://jobs.opensafely.org/investigating-the-effectiveness-of-the-covid-19-vaccination-programme-in-the-uk-project-continuation-of-approved-project-no-22/
@@ -25,7 +27,12 @@ PROJECTS_WITH_NDOO_PERMISSION = {
 }
 
 
-def project_is_permitted_to_use_ndoo_data(project):
+def project_has_permission(project):
     if not project.number:
         return False
     return project.number in PROJECTS_WITH_NDOO_PERMISSION
+
+
+def analysis_scope_for_project(project):
+    if project_has_permission(project):
+        return ANALYSIS_SCOPE_KEY
