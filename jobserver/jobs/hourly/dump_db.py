0d3a67eb466744aa85de5280371fe0b3d3413421
Nishtha <nishtha.kalra25@gmail.com>
2025-12-02 14:56:08 +0000

    Rename script and dump
    Put back original hourly script and rename the daily script so that we
    could test that the new script works end to end and the hourly script
    keeps running as it is.
    Will replace the hourly script with daily once testing is done

---
+++ jobserver/jobs/hourly/dump_db.py
@@ -0,0 +1,37 @@
+import pathlib
+import subprocess
+import sys
+
+from django.conf import settings
+from django_extensions.management.jobs import HourlyJob
+from sentry_sdk.crons.decorator import monitor
+
+from services.sentry import monitor_config
+
+
+class Job(HourlyJob):
+    help = "Dump the database to storage for copying to local dev environments"  # noqa: A003
+
+    @monitor(monitor_slug="dump_db", monitor_config=monitor_config("0 * * * *"))
+    def execute(self):
+        db = settings.DATABASES["default"]
+        output = pathlib.Path("/storage/jobserver.dump")
+
+        if not output.exists():
+            print(f"Unknown output path: {output}", file=sys.stderr)
+            sys.exit(1)
+
+        # We use the "custom" output format for pg_dump to get smaller
+        # files, the docs have more detail on why it can be useful:
+        # https://www.postgresql.org/docs/14/app-pgdump.html
+        subprocess.check_call(
+            [
+                "pg_dump",
+                "--format=c",
+                "--no-acl",
+                "--no-owner",
+                f"--file={output}",
+                f"postgresql://{db['USER']}@{db['HOST']}:{db['PORT']}/{db['NAME']}",
+            ],
+            env={"PGPASSWORD": db["PASSWORD"]},
+        )
