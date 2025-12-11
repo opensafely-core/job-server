1c20d80e9112ce142563e802d341b2c39abd431e
Steven Maude <git@stevenmaude.co.uk>
2025-12-11 23:33:57 +0000

    Add a simple Playwright page load test
    References:
    
    * opensafely-core/airlock@6ca506a083a0323f719e615c991edf02c7cb3233
    * opensafely-core/airlock@039e1d148bcc5c4e9dad691bb92a97769f232ced

---
+++ tests/functional/conftest.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import sys
+
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env():
+    # This is required for playwright tests with Django
+    # See https://github.com/microsoft/playwright-pytest/issues/29
+    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "1"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def playwright_install(request):
+    # As this can potentially take a long time when it's first run (and as it is
+    # subsequently a silent no-op) we disable output capturing so that progress gets
+    # displayed to the user
+    capmanager = request.config.pluginmanager.getplugin("capturemanager")
+    command = [sys.executable, "-m", "playwright", "install", "chromium"]
+    # Install with dependencies in CI (but not in docker, as they've already
+    # been installed in the image)
+    if os.environ.get("CI") and not os.environ.get("DOCKER"):  # pragma: no cover
+        command.extend(["--with-deps"])
+    with capmanager.global_and_fixture_disabled():
+        subprocess.run(command, check=True)
+
+
+# Selectively allow functional tests to bypass pytest-socket.
+# Otherwise, pytest-socket blocks access and Playwright tests fail.
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        if "functional" in item.keywords:
+            item.add_marker(pytest.mark.allow_hosts(["127.0.0.1", "::1"]))
