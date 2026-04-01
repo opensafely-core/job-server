3513d91eb2cac7fd1d053748432f82468ab5b7ad
Mike Kelly <mike.kelly@thedatalab.org>
2026-04-01 15:04:03 +0100

    Add PROJECT_ROLES and ALL_ROLES, fix tests.
    To mirror GLOBAL_ROLES. These are not used yet but I intend to use them in
    future commits, breaking up the changes.
    
    The tests were using the & intersection set operator, which tests for
    intersection, not equality. Checking for equality seems more robust. Therefore
    the test was missing some new roles. Added these.

--- jobserver/authorization/global_roles.py
+++ jobserver/authorization/global_roles.py
@@ -1,10 +1,14 @@
-from ..models import User
+from ..models import ProjectMembership, User
 from .utils import roles_for
 
+
+# Not included in __init__ to avoid circular import problems.
 
 # A global role can be assigned to a User independently of ProjectMembership.
 # It either does not relate to Projects or grants permissions across ALL projects.
 
 GLOBAL_ROLES = roles_for(User)
-
 GLOBAL_ROLE_NAMES = [role.__name__ for role in GLOBAL_ROLES]
+
+PROJECT_ROLES = roles_for(ProjectMembership)
+ALL_ROLES = set(GLOBAL_ROLES).union(set(PROJECT_ROLES))
--- tests/unit/jobserver/authorization/test_global_roles.py
+++ tests/unit/jobserver/authorization/test_global_roles.py
@@ -1,14 +1,68 @@
-from jobserver.authorization.global_roles import GLOBAL_ROLE_NAMES
+import jobserver.authorization.roles as roles_module
+from jobserver.authorization.global_roles import (
+    ALL_ROLES,
+    GLOBAL_ROLE_NAMES,
+    GLOBAL_ROLES,
+    PROJECT_ROLES,
+)
+from jobserver.authorization.roles import (
+    DeploymentAdministrator,
+    OutputChecker,
+    OutputPublisher,
+    ProjectCollaborator,
+    ProjectDeveloper,
+    ServiceAdministrator,
+    SignOffRepoWithOutputs,
+    StaffAreaAdministrator,
+    TechSupport,
+)
 
 
 def test_global_role_names():
-    assert set(GLOBAL_ROLE_NAMES) & set(
+    assert set(GLOBAL_ROLE_NAMES) == set(
         [
-            "DeploymentAdministrator",
+            "StaffAreaAdministrator",
+            "TechSupport",
+            "ServiceAdministrator",
             "OutputChecker",
             "OutputPublisher",
             "ProjectCollaborator",
             "SignOffRepoWithOutputs",
-            "StaffAreaAdministrator",
+            "DeploymentAdministrator",
         ]
     )
+
+
+def test_global_roles():
+    assert set(GLOBAL_ROLES) == set(
+        [
+            StaffAreaAdministrator,
+            TechSupport,
+            ServiceAdministrator,
+            OutputChecker,
+            OutputPublisher,
+            ProjectCollaborator,
+            SignOffRepoWithOutputs,
+            DeploymentAdministrator,
+        ]
+    )
+
+
+def test_project_roles():
+    assert set(PROJECT_ROLES) == set(
+        [
+            ProjectDeveloper,
+            ProjectCollaborator,
+        ]
+    )
+
+
+def test_all_roles():
+    """Test that ALL_ROLES has all the roles from the roles module."""
+    role_names_in_module = {
+        role_name
+        for role_name in dir(roles_module)
+        # Exclude private attributes and the imported Permission class.
+        if not role_name.startswith("_") and not role_name.startswith("Perm")
+    }
+    assert {role.__name__ for role in ALL_ROLES} == role_names_in_module
