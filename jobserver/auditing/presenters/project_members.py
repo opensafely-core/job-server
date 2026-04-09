6fa9780fecc904cfc81f239b575ed43ad7eeda37
Katie Bickford <katie.bickford@thedatalab.org>
2026-04-09 13:07:10 +0100

    Update autit log events to use display name format
    Audit events store `created_by` and `target_user` as text fields,
    so we cannot directly use teh `get_display_name()` User model method in templates.
    
    This commit updates presenters to pass user objects into the event context as
    `actor_user_obj` and `user_obj`, so that we can update our templates to use `get_display_name()`
    for consistent rendering of users as "fullname (username)".

--- jobserver/auditing/presenters/project_members.py
+++ jobserver/auditing/presenters/project_members.py
@@ -3,50 +3,59 @@ from .utils import lookup_project, lookup_user, roles_str_to_class_name_str
 
 
 def added(*, event):
+    actor_user_obj = lookup_user(event.created_by)
     actor = LinkableObject.build(
-        obj=lookup_user(event.created_by),
+        obj=actor_user_obj,
         link_func="get_staff_url",
     )
     project = LinkableObject.build(
         obj=lookup_project(pk=event.parent_id),
         link_func="get_staff_url",
     )
+    user_obj = lookup_user(event.target_user)
     user = LinkableObject.build(
-        obj=lookup_user(event.target_user),
+        obj=user_obj,
         link_func="get_staff_url",
     )
 
     return PresentableAuditableEvent(
         context={
             "actor": actor,
+            "actor_user_obj": actor_user_obj,
             "created_at": event.created_at,
             "project": project,
             "user": user,
+            "user_obj": user_obj,
         },
         template_name="staff/auditable_events/project/members/added.html",
     )
 
 
 def updated_roles(*, event):
+    actor_user_obj = lookup_user(event.created_by)
+
     actor = LinkableObject.build(
-        obj=lookup_user(event.created_by),
+        obj=actor_user_obj,
         link_func="get_staff_url",
     )
     project = LinkableObject.build(
         obj=lookup_project(pk=event.parent_id),
         link_func="get_staff_url",
     )
+    user_obj = lookup_user(event.target_user)
     user = LinkableObject.build(
-        obj=lookup_user(event.target_user),
+        obj=user_obj,
         link_func="get_staff_url",
     )
 
     return PresentableAuditableEvent(
         context={
             "actor": actor,
+            "actor_user_obj": actor_user_obj,
             "created_at": event.created_at,
             "project": project,
             "user": user,
+            "user_obj": user_obj,
             "before": roles_str_to_class_name_str(event.old),
             "after": roles_str_to_class_name_str(event.new),
         },
@@ -55,25 +64,30 @@ def updated_roles(*, event):
 
 
 def removed(*, event):
+    actor_user_obj = lookup_user(event.created_by)
+
     actor = LinkableObject.build(
-        obj=lookup_user(event.created_by),
+        obj=actor_user_obj,
         link_func="get_staff_url",
     )
     project = LinkableObject.build(
         obj=lookup_project(pk=event.parent_id),
         link_func="get_staff_url",
     )
+    user_obj = lookup_user(event.target_user)
     user = LinkableObject.build(
-        obj=lookup_user(event.target_user),
+        obj=user_obj,
         link_func="get_staff_url",
     )
 
     return PresentableAuditableEvent(
         context={
             "actor": actor,
+            "actor_user_obj": actor_user_obj,
             "created_at": event.created_at,
             "project": project,
             "user": user,
+            "user_obj": user_obj,
         },
         template_name="staff/auditable_events/project/members/removed.html",
     )
