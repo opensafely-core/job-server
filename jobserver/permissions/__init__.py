46e239078516e81476d02bd4a7edd1a646090979
Becky Smith <becky.smith@thedatalab.org>
2026-05-20 13:47:18 +0100

    Fix imports and add a test for them
    Adds a test to ensure that all the expected permissions modules
    are included in a permissions submodule's __all__

--- jobserver/permissions/__init__.py
+++ jobserver/permissions/__init__.py
@@ -1,5 +1,4 @@
 from . import sqlrunner, validator
-from .population_permissions import t1oo
 
 
-__all__ = ["sqlrunner", "t1oo", "validator"]
+__all__ = ["sqlrunner", "validator"]
--- tests/unit/jobserver/permissions/test_permissions.py
+++ tests/unit/jobserver/permissions/test_permissions.py
@@ -1,3 +1,6 @@
+import importlib
+import pkgutil
+
 import pytest
 
 from jobserver import permissions
@@ -26,3 +29,24 @@ from jobserver import permissions
 )
 def test_project_permission_vars(value):
     permissions.validator.validate_project_identifier_in_permissions(value)
+
+
+def test_all_permission_modules_in_all():
+    # Prevent accidentally omitting a new permissions module from its subpackage's __all__
+    missing = {}
+    for _, subpkg_name, is_pkg in pkgutil.iter_modules(permissions.__path__):
+        if not is_pkg:
+            continue
+        subpkg = importlib.import_module(f"jobserver.permissions.{subpkg_name}")
+        subpkg_all = set(getattr(subpkg, "__all__", []))
+        for _, mod_name, _ in pkgutil.iter_modules(subpkg.__path__):
+            if mod_name not in subpkg_all:  # pragma: no cover
+                missing.setdefault(subpkg_name, []).append(mod_name)
+
+    assert not missing, (
+        "Missing from __all__ in permissions module's __init__.py:\n"
+        + "\n".join(
+            f"  permissions.{subpkg_name}: {', '.join(mods)}"
+            for subpkg_name, mods in missing.items()
+        )
+    )
