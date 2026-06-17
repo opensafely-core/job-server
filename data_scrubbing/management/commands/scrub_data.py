6052de5a3cbd8bf1569083a03d219759c7197788
Mike Kelly <mike.kelly@thedatalab.org>
2026-06-17 12:29:26 +0100

    Wrap database edits in a transaction.
    This avoids some error leading to a half scrubbed database. Either all or no
    relevant fields will be updated.
    
    It does not matter too much in practice as this is running in an database only
    for this purpose that we expect to be empty between runs.

--- data_scrubbing/management/commands/scrub_data.py
+++ data_scrubbing/management/commands/scrub_data.py
@@ -1,5 +1,6 @@
 from django.apps import apps
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from faker import Faker
 
 
@@ -14,25 +15,26 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         models = {model for model in apps.get_app_config("jobserver").get_models()}
         models |= {model for model in apps.get_app_config("applications").get_models()}
+        with transaction.atomic():
+            for model in models:
+                data_scrubbing = getattr(model, "DataScrubbing", None)
+                if data_scrubbing is None:
+                    continue
+                scrub_fields = getattr(data_scrubbing, "fields_to_scrub", None)
+                if not scrub_fields:
+                    continue
+                field_names = scrub_fields.keys()
 
-        for model in models:
-            data_scrubbing = getattr(model, "DataScrubbing", None)
-            if data_scrubbing is None:
-                continue
-            scrub_fields = getattr(data_scrubbing, "fields_to_scrub", None)
-            if not scrub_fields:
-                continue
-            field_names = scrub_fields.keys()
+                objs = model.objects.all().iterator()
+                count = 0
+                for obj in objs:
+                    for attr_name, fake_value in scrub_fields.items():
+                        value = fake_value() if callable(fake_value) else fake_value
+                        setattr(obj, attr_name, value)
+                    obj.save(update_fields=field_names)
+                    count += 1
 
-            objs = model.objects.all().iterator()
-            count = 0
-            for obj in objs:
-                for attr_name, fake_value in scrub_fields.items():
-                    value = fake_value() if callable(fake_value) else fake_value
-                    setattr(obj, attr_name, value)
-                obj.save(update_fields=field_names)
-                count += 1
-
-            self.stdout.write(
-                f"Scrubbed {count} {model.__name__} records from fields: {', '.join(field_names)}"
-            )
+                self.stdout.write(
+                    f"Scrubbed {count} {model.__name__} records from fields: {', '.join(field_names)}"
+                )
+        self.stdout.write("Committed scrubbing transaction")
