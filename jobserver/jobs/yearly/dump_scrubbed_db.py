5c3e4fdfa51cfc4f953ddc6945c128d6dc157073
Nishtha <nishtha.kalra25@gmail.com>
2026-06-25 17:56:17 +0100

    Move logs into helpers to improve readability

--- jobserver/jobs/yearly/dump_scrubbed_db.py
+++ jobserver/jobs/yearly/dump_scrubbed_db.py
@@ -16,7 +16,7 @@ class Job(YearlyJob):
     help = "Scrubbed database dump job (in progress)"
 
     def execute(self):
-        scrubbed_dump_path = pathlib.Path(settings.SCRUBBED_DATABASE_DUMP_PATH)
+        scrubbed_dump_path = settings.SCRUBBED_DATABASE_DUMP_PATH
         readonly_database = settings.DATABASES.get("readonly")
         data_scrubbing_database = settings.DATABASES.get("data_scrubbing")
 
@@ -32,36 +32,14 @@ class Job(YearlyJob):
             temp_dir = pathlib.Path(temp_dir)
             raw_dump_path = temp_dir / "raw.dump"
             temp_scrubbed_dump_path = temp_dir / scrubbed_dump_path.name
-            logger.info(
-                "Creating raw dump of readonly jobserver database", path=raw_dump_path
-            )
             dump_database(readonly_database, raw_dump_path)
-            logger.info(
-                "Finished creating raw dump of readonly jobserver database",
-                path=raw_dump_path,
-            )
             try:
-                logger.info("Restoring dump into data scrubbing database")
                 restore_database(data_scrubbing_database, raw_dump_path)
-                logger.info("Finished restoring dump into database")
-
-                logger.info("Scrubbing data scrubbing database")
-                call_command("scrub_data", "data_scrubbing")
-                logger.info("Finished scrubbing database")
-
-                logger.info(
-                    "Creating scrubbed database dump", path=temp_scrubbed_dump_path
-                )
+                scrub_database("data_scrubbing")
                 dump_database(data_scrubbing_database, temp_scrubbed_dump_path)
                 temp_scrubbed_dump_path.replace(scrubbed_dump_path)
-                logger.info(
-                    "Finished creating scrubbed database dump",
-                    path=scrubbed_dump_path,
-                )
             finally:
-                logger.info("Clearing data scrubbing database")
                 clear_database(data_scrubbing_database)
-                logger.info("Finished clearing data scrubbing database")
 
 
 def database_connection_args(database_config):
@@ -98,6 +76,7 @@ def run_database_command(command, database_config):
 
 
 def dump_database(database_config, output):
+    logger.info("Creating database dump", path=output)
     run_database_command(
         [
             "pg_dump",
@@ -109,9 +88,13 @@ def dump_database(database_config, output):
         ],
         database_config,
     )
+    logger.info("Finished creating database dump", path=output)
 
 
 def restore_database(database_config, input_dump):
+    logger.info(
+        "Restoring database dump", path=input_dump, database=database_config["NAME"]
+    )
     run_database_command(
         [
             "pg_restore",
@@ -124,9 +107,21 @@ def restore_database(database_config, input_dump):
         ],
         database_config,
     )
+    logger.info(
+        "Finished restoring dump into database",
+        path=input_dump,
+        database=database_config["NAME"],
+    )
+
+
+def scrub_database(database_alias):
+    logger.info("Scrubbing database", database=database_alias)
+    call_command("scrub_data", database_alias)
+    logger.info("Finished scrubbing database", database=database_alias)
 
 
 def clear_database(database_config):
+    logger.info("Clearing database", database=database_config["NAME"])
     run_database_command(
         [
             "psql",
@@ -136,3 +131,4 @@ def clear_database(database_config):
         ],
         database_config,
     )
+    logger.info("Finished clearing database", database=database_config["NAME"])
