c32f960169ec3e4c313f115254a817969711f31e
Mike Kelly <mike.kelly@thedatalab.org>
2026-07-15 13:34:06 +0100

    Refactor: Move `social_django` factories to own dir.
    That will distinguish them clearly from the ones that we define, avoiding some
    possible confusion, clutter, and clashes.

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -6,7 +6,6 @@ from .job import *  # noqa: F401, F403
 from .job_request import *  # noqa: F401, F403
 from .org import *  # noqa: F401, F403
 from .org_membership import *  # noqa: F401, F403
-from .partial import *  # noqa: F401, F403
 from .project import *  # noqa: F401, F403
 from .project_collaboration import *  # noqa: F401, F403
 from .publish_request import *  # noqa: F401, F403
@@ -18,7 +17,8 @@ from .release_file_review import *  # noqa: F401, F403
 from .repo import *  # noqa: F401, F403
 from .site_alert import *  # noqa: F401, F403
 from .snapshot import *  # noqa: F401, F403
+from .social_django.partial import *  # noqa: F401, F403
+from .social_django.user_social_auth import *  # noqa: F401, F403
 from .stats import *  # noqa: F401, F403
 from .user import *  # noqa: F401, F403
-from .user_social_auth import *  # noqa: F401, F403
 from .workspace import *  # noqa: F401, F403
---
+++ tests/factories/social_django/__init__.py
@@ -0,0 +1,2 @@
+"""Contains factories from social_django.models so that they are separated from
+the factories for models we define."""
