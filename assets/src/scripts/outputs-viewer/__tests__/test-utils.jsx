d6c6217c48dcb4622b7f48b500bd850bcf644d6c
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2026-07-31 11:42:47 +0100

    Upgrade React Router to v8

--- assets/src/scripts/outputs-viewer/__tests__/test-utils.jsx
+++ assets/src/scripts/outputs-viewer/__tests__/test-utils.jsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import * as React from "react";
-import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router";
 
 export const history = createMemoryHistory({ window });
 
