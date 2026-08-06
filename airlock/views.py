fca811923286b678b04c3d004a58c9d4a23eaec9
Steven Maude <git@stevenmaude.co.uk>
2026-08-06 13:38:40 +0000

    Enable rule PIE796 in Ruff
    "PIE796 Enum contains duplicate value: …"
    
    The one example seems intentional.

--- airlock/views.py
+++ airlock/views.py
@@ -36,7 +36,7 @@ class EventType(Enum):
     REQUEST_RETURNED = "request returned"
     REQUEST_RESUBMITTED = "request resubmitted"
     REQUEST_PARTIALLY_REVIEWED = "request reviewed"
-    REQUEST_REVIEWED = "request reviewed"
+    REQUEST_REVIEWED = "request reviewed"  # noqa: PIE796
 
     def status_label(self):
         """The GitHub Issue label that should be added for this request"""
