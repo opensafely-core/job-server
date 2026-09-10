ec8e1286c6f8114d61a51919fa0a0435dbfba56d
Mike Kelly <mike.kelly@thedatalab.org>
2026-09-10 16:24:10 +0100

    Move deployment checks and migrations to `release` process.
    This commit moves Django checks and database migrations earlier in the deployment
    phase, to the special `release` process. This fixes
    https://github.com/opensafely-core/job-server/issues/4994 and is what Dokku
    recommend:
    
    https://dokku.com/docs/advanced-usage/deployment-tasks/#procfile-release-command
    
    > The Procfile also supports a special release command which acts in a similar
    > way to the Heroku Release Phase. This command is executed after an app's
    > docker image is built, but before any containers are scheduled.
    
    This is done by adding a `release` key to the `Procfile` pointing at the
    `prod.sh` script and removing the `ENTRYPOINT` from the `Dockerfile`. The dev
    Docker image overwrites the `ENTRYPOINT` and so is unaffected by its removal.
    The `CMD` in the Dockerfile remains, allowing for overriding of the command
    when running the container.  The shebang line in the script has had
    `/usr/bin/env` added for consistency with opencodelists and to ensure that the
    spawned process has the right environment.
    
    https://github.com/opensafely-core/opencodelists/commit/66e6228ae5f84672137c8f9a822d8c86bd9857c7
    
    This means the migrations are run once per attempted deploy and the deploy
    stops if the  migrations fail, so no other process is launched. We think that
    has better properties. There was an outage where migrations were not fully
    applied during a deployment and the app needed to be redeployed in order for
    the migrations to be applied fully.
    https://docs.google.com/document/d/1L9qMOAOe65Fwl7pHkbbCJ0otRsOEQvNHc-O68U-6rvg/edit?tab=t.0#heading=h.t54dozhltz9b
    
    Not running the migrations during the web container startup can get it healthy
    more quickly. That can avoid losing a race with health checks that expect the
    container to respond within 60 seconds. Migrations may take arbitrarily long,
    maybe too long for healthchecks to pass, causing the deploy to fail but still
    changing the database.
    
    There is still a window where the old containers are running against the
    migrated database until the healthchecks pass and the zero-downtime deploy
    switched over. This means that the advice on risky migrations in our
    DEVELOPERS.md documentation still applies.

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: /usr/bin/env bash /docker/entrypoints/prod.sh
 web: gunicorn --config gunicorn.conf.py jobserver.wsgi
 rapstatus: python ./manage.py rap_status_service
--- docker/entrypoints/prod.sh
+++ docker/entrypoints/prod.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Commands run during the release phase after build but before deploy.
+# https://dokku.com/docs/processes/process-management/#the-release-process
+# https://devcenter.heroku.com/articles/release-phase
 
 set -euo pipefail
 
 ./manage.py check --deploy
 ./manage.py migrate
-
-exec "$@"
