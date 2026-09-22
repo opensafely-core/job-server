168b52f8f93cd8b2e0a42f6ed18e4d96b0ed2815
Mike Kelly <mike.kelly@thedatalab.org>
2026-09-22 12:19:36 +0100

    Remove `deployment/release_phase`.
    It is simpler to just put the release phase commands in the Procfile.
    
    In production, we want the release phase container to run the checks and
    migrations, and the web and rapstatus containers to only run their process. So
    we should not have an ENTRYPOINT that always runs the checks. Document that.
    
    diff --git c/Procfile i/Procfile index b5fa97b9..4f20a163 100644 --- c/Procfile
    +++ i/Procfile @@ -1,3 +1,3 @@ -release: /usr/bin/env bash
    /app/deployment/release_phase.sh +release: python ./manage.py check --deploy &&
    python ./manage.py migrate web: gunicorn --config gunicorn.conf.py
    jobserver.wsgi rapstatus: python ./manage.py rap_status_service diff --git
    c/deployment/release_phase.sh i/deployment/release_phase.sh deleted file mode
    100755 index 1441ea8e..00000000 --- c/deployment/release_phase.sh +++ /dev/null
    @@ -1,9 +0,0 @@ -#!/usr/bin/env bash -# Commands run during the release phase
    after build but before deploy.  -#
    https://dokku.com/docs/processes/process-management/#the-release-process -#
    https://devcenter.heroku.com/articles/release-phase
    -
    -set -euo pipefail
    -
    -./manage.py check --deploy -./manage.py migrate diff --git c/docker/Dockerfile
    i/docker/Dockerfile index 87347274..f114fac2 100644 --- c/docker/Dockerfile +++
    i/docker/Dockerfile @@ -231,8 +231,10 @@ RUN SECRET_KEY=dummy-key \
    RAP_API_TOKEN=dummy \ python /app/manage.py collectstatic --no-input
    
    -# We set command rather than entrypoint, to make it easier to run different
    -# things from the cli
    +# We set CMD rather than ENTRYPOINT, as different processes want to run
    +# different commands. The CMD command can be overridden when running a
    +# container from the CLI. In production, Dokku arranges for the
    +# container-specific Procfile command to override the CMD.
     CMD ["gunicorn", "--config", "gunicorn.conf.py", "jobserver.wsgi"]
    
     # finally, tag with build information. These will change regularly, therefore

--- deployment/release_phase.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Commands run during the release phase after build but before deploy.
-# https://dokku.com/docs/processes/process-management/#the-release-process
-# https://devcenter.heroku.com/articles/release-phase
-
-set -euo pipefail
-
-./manage.py check --deploy
-./manage.py migrate
